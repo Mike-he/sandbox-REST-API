c4f46564861fa660a8616db949cb91ce190212b7
Yimo Zhang <yimozh@gobeta.com.cn>
2015-06-16 16:06:55 +0800

    re #10838, change ClientApiAuthView condition

--- contrib/views/client/create_client_api_auth_view.sql
+++ contrib/views/client/create_client_api_auth_view.sql
@@ -8,15 +8,5 @@ SELECT
 FROM UserToken AS t
 JOIN User AS u ON t.username = u.username
 WHERE
-    -- current timestamp in millisecond
-    UNIX_TIMESTAMP() * 1000
-    <
-    -- less than the expiration date
-    -- which is 14 days
-    (
-        CAST(t.creationDate AS UNSIGNED)
-        + 
-        (14*24*60*60*1000)
-    )
-    AND u.status = 'activated'
+    t.creationDate > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 DAY)
 ;
