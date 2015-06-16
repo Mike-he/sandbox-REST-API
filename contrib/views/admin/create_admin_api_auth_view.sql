2795af97a8d506eaf7c4e34d09da10ee6bdb7376
Yimo Zhang <yimozh@gobeta.com.cn>
2015-06-16 14:37:45 +0800

    close #10723, change the date compare for admin api auth view

--- contrib/views/admin/create_admin_api_auth_view.sql
+++ contrib/views/admin/create_admin_api_auth_view.sql
@@ -8,14 +8,5 @@ SELECT
 FROM AdminToken AS t
 JOIN Admin AS a ON t.username = a.username
 WHERE
-    -- current timestamp in millisecond
-    UNIX_TIMESTAMP() * 1000
-    <
-    -- less than the expiration date
-    -- which is 14 days
-    (
-        CAST(t.creationDate AS UNSIGNED)
-        + 
-        (14*24*60*60*1000)
-    )
+    t.creationDate > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 5 DAY)
 ;
