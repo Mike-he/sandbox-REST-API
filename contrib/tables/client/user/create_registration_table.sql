e3a4b614197e91debc6bcf08e5bb192b36bb6556
josh <joshy@gobeta.com.cn>
2015-06-18 14:56:13 +0800

    re #10732 update create table query

--- contrib/tables/client/user/create_registration_table.sql
+++ contrib/tables/client/user/create_registration_table.sql
@@ -1,10 +1,10 @@
-CREATE  TABLE `UserRegistration` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `phone` VARCHAR(64) NULL ,
-  `email` VARCHAR(128) NULL ,
-  `code` VARCHAR(16) NULL ,
-  `creationDate` VARCHAR(15) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE KEY `uk_UserRegistration__phone` (`phone`) ,
+CREATE TABLE `UserRegistration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(64) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `code` varchar(16) DEFAULT NULL,
+  `creationDate` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_UserRegistration__phone` (`phone`),
   UNIQUE KEY `uk_UserRegistration__email` (`email`)
-) ;
+);
