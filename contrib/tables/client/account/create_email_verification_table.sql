5dce8ba8f34be176bdd01bcb2b6f3338b2566912
josh <joshy@gobeta.com.cn>
2015-06-24 12:04:36 +0800

    re #10947 update create table query

--- contrib/tables/client/account/create_email_verification_table.sql
+++ contrib/tables/client/account/create_email_verification_table.sql
@@ -2,9 +2,7 @@ CREATE TABLE `EmailVerification` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `token` varchar(64) NOT NULL,
   `code` varchar(16) NOT NULL,
   `creationDate` char(15) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token_UNIQUE` (`token`)
+  PRIMARY KEY (`id`)
 );
