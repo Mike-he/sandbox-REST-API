5390cc5835fb3a47b12a59a5b028990dfed833a0
josh <joshy@gobeta.com.cn>
2015-06-25 14:27:36 +0800

    re #10949 update create table query

--- contrib/tables/client/account/create_forget_pwd_table.sql
+++ contrib/tables/client/account/create_forget_pwd_table.sql
@@ -1,11 +1,13 @@
 CREATE TABLE `ForgetPassword` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `token` varchar(64) NOT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `phone` varchar(64) DEFAULT NULL,
+  `token` varchar(64) DEFAULT NULL,
   `code` varchar(16) NOT NULL,
   `status` enum('submit','verify') NOT NULL,
   `type` enum('email','phone') NOT NULL,
-  `creationDate` char(15) NOT NULL,
+  `creationDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token_UNIQUE` (`token`)
 );
