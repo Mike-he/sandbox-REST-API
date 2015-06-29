278dad2c1dad3bffcd715fdfc93c3c0f8afbf11f
josh <joshy@gobeta.com.cn>
2015-06-29 18:36:42 +0800

    re #11005 update and add create tables query. close # Josh`s story is over :), see u, guys

---
+++ contrib/tables/client/user/create_user_portfolio_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `UserPortfolio` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`userId` int(11) NOT NULL,
+`content` text NOT NULL,
+`attachmentType` varchar(64) NOT NULL,
+`fileName` varchar(64) NOT NULL,
+`preview` text,
+`size` int(11) NOT NULL,
+`creationDate` datetime NOT NULL,
+`modificationDate` datetime NOT NULL,
+PRIMARY KEY (`id`),
+KEY `fk_UserPortfolio_userId_idx` (`userId`)
+);
