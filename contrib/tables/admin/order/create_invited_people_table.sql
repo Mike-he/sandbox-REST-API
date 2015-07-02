e5a9270462f537f557dd50d2628a943bd036f4c6
Leo Xu <leox@gobeta.com.cn>
2015-07-02 12:37:52 +0800

    re #11115, update product table and entity

--- contrib/tables/admin/order/create_invited_people_table.sql
+++ contrib/tables/admin/order/create_invited_people_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE `InvitedPeople` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `orderId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_InvitedPeople_orderId_idx` (`orderId`),
   CONSTRAINT `fk_InvitedPeople_orderId` FOREIGN KEY (`orderId`) REFERENCES `ProductOrder` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
