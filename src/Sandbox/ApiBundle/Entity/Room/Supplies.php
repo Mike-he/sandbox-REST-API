bbfc545d03988b46ac5453ec243a2cd7d581e71c
Sergi Uceda <sergiu@gobeta.com.cn>
2015-07-02 15:37:56 +0800

    api to get the offices supplies list

---
+++ src/Sandbox/ApiBundle/Entity/Room/Supplies.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Sandbox\ApiBundle\Entity\Room;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Supplies.
+ *
+ * @ORM\Table(
+ *      name="Supplies",
+ *      uniqueConstraints={@ORM\UniqueConstraint(name="name_UNIQUE", columns={"name"})}
+ * )
+ * @ORM\Entity
+ */
+class Supplies
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return Supplies
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
