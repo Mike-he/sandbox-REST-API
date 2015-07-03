928e6adfe7cafd041ece00aa9846fa7522c323b1
Yimo Zhang <yimozh@gobeta.com.cn>
2015-07-03 10:38:43 +0800

    re #10733, factorize admin create

--- src/Sandbox/AdminApiBundle/Controller/Admin/AdminAdminsController.php
+++ src/Sandbox/AdminApiBundle/Controller/Admin/AdminAdminsController.php
@@ -2,15 +2,17 @@
 
 namespace Sandbox\AdminApiBundle\Controller\Admin;
 
-use Sandbox\ApiBundle\Controller\Admin\AdminLoginController;
+use Sandbox\ApiBundle\Controller\SandboxRestController;
 use Sandbox\ApiBundle\Entity\Admin\Admin;
 use Sandbox\ApiBundle\Entity\Admin\AdminPermissionMap;
-use Sandbox\ApiBundle\Form\Admin\AdminType;
+use Sandbox\ApiBundle\Form\Admin\AdminType as AdminForm;
+use Sandbox\ApiBundle\Entity\Admin\AdminType;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
@@ -23,7 +25,7 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
  *
  * @link     http://www.Sandbox.cn/
  */
-class AdminAdminsController extends AdminLoginController
+class AdminAdminsController extends SandboxRestController
 {
     /**
      * Create admin.
@@ -49,27 +51,82 @@ class AdminAdminsController extends AdminLoginController
     ) {
         // TODO a common function to check user permission
 
-        $adminUser = $this->getUser();
+        $user = $this->getUser();
+        $myAdmin = $this->getRepo('Admin\Admin')->find($user->getAdminId());
 
+        // only super admin is allowed to create admin account
+        $type = $this->getRepo('Admin\AdminType')->findOneByKey(AdminType::KEY_SUPER);
+        if ($type->getId() != $myAdmin->getTypeId()) {
+            throw new AccessDeniedHttpException(self::NOT_ALLOWED_MESSAGE);
+        }
+
+        return $this->handleAdminCreate($request);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return View
+     */
+    private function handleAdminCreate(
+        $request
+    ) {
+        // handle admin create data
+        $admin = $this->handleAdminCreateData($request);
+
+        // save admin to db
+        $admin = $this->saveAdmin($admin);
+
+        $view = new View();
+        $view->setData(array(
+            'id' => $admin->getId(),
+        ));
+
+        return $view;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Admin
+     */
+    private function handleAdminCreateData(
+        $request
+    ) {
         $admin = new Admin();
 
-        // bind client data
-        $form = $this->createForm(new AdminType(), $admin);
+        // bind admin data
+        $form = $this->createForm(new AdminForm(), $admin);
         $form->handleRequest($request);
 
         if (!$form->isValid()) {
             throw new BadRequestHttpException(self::BAD_PARAM_MESSAGE);
         }
 
+        // TODO return custom error
+
+
+        return $admin;
+    }
+
+    /**
+     * @param Admin $admin
+     *
+     * @return Admin
+     */
+    private function saveAdmin(
+        $admin
+    ) {
+        // set type
         $type = $this->getRepo('Admin\AdminType')->find($admin->getTypeId());
         if (is_null($type)) {
             $this->throwNotFoundIfNull($type, self::NOT_FOUND_MESSAGE);
         }
-
         $admin->setType($type);
 
         $now = new \DateTime('now');
 
+        // set permissions
         $permissionIds = array();
         foreach ($admin->getPermissions() as $permission) {
             $permissionId = $permission['id'];
@@ -88,20 +145,15 @@ class AdminAdminsController extends AdminLoginController
         }
         $admin->setPermissionIds($permissionIds);
 
+        // set dates
         $admin->setCreationDate($now);
         $admin->setModificationDate($now);
 
-        var_dump($admin);
-
+        // save admin
         $em = $this->getDoctrine()->getManager();
         $em->persist($admin);
         $em->flush();
 
-        $view = new View();
-        $view->setData(array(
-           'id' => $admin->getId(),
-        ));
-
-        return $view;
+        return $admin;
     }
 }
