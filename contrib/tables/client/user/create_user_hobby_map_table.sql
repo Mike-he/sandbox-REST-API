fce8a3f85ffd84d766edf93362370f826b55cfd0
Yimo Zhang <yimozh@gobeta.com.cn>
2015-07-06 08:56:08 +0800

    re #11105, fix create table queries

--- contrib/tables/client/user/create_user_hobby_map_table.sql
+++ contrib/tables/client/user/create_user_hobby_map_table.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `UserHobbyMap` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`userId` int(11) NOT NULL,
-`hobbyId` int(11) NOT NULL,
-PRIMARY KEY (`id`),
-KEY `fk_UserHobbiesMap_userId_idx` (`userId`),
-KEY `fk_UserHobbiesMap_hobbyId_idx` (`hobbyId`),
-CONSTRAINT `fk_UserHobbyMap_userId` FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-CONSTRAINT `fk_UserHobbyMap_hobbyId` FOREIGN KEY (`hobbyId`) REFERENCES `Hobby` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `hobbyId` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userId_hobbyId_UNIQUE` (`userId`,`hobbyId`),
+  KEY `fk_UserHobbiesMap_userId_idx` (`userId`),
+  KEY `fk_UserHobbiesMap_hobbyId_idx` (`hobbyId`),
+  CONSTRAINT `fk_UserHobbyMap_userId` FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_UserHobbyMap_hobbyId` FOREIGN KEY (`hobbyId`) REFERENCES `Hobby` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 );
