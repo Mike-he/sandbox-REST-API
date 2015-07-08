cc469eabdd1243a4d124b01345e748dee135f84f
Albert FENG <albert.feng@easylinks.com.cn>
2015-07-08 12:14:13 +0800

    re #11213 add create queries.

--- contrib/tables/client/company/create_company_industry_table.sql
+++ contrib/tables/client/company/create_company_industry_table.sql
@@ -1,15 +1,7 @@
-CREATE TABLE `Company` (
+CREATE TABLE `CompanyIndustry` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
-  `address` varchar(1024) DEFAULT NULL,
-  `website` varchar(256) DEFAULT NULL,
-  `phone` varchar(32) DEFAULT NULL,
-  `fax` varchar(32) DEFAULT NULL,
-  `description` mediumtext CHARACTER SET utf8mb4,
-  `creatorId` varchar(64) NOT NULL,
-  `creationDate` char(15) NOT NULL,
-  `modificationDate` char(15) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `creatorId_idx` (`creatorId`),
-  CONSTRAINT `fk_company_creatorId` FOREIGN KEY (`creatorId`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `name` varchar(64) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 );
--- contrib/tables/client/company/create_company_member.sql
+++ contrib/tables/client/company/create_company_member.sql
@@ -1,12 +1,14 @@
 CREATE TABLE `CompanyMember` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` varchar(64) NOT NULL,
+  `userId` int(11) NOT NULL,
   `companyId` int(11) NOT NULL,
-  `isDelete` tinyint(4) NOT NULL DEFAULT '0',
+  `creationDate` datetime NOT NULL,
+  `modificationDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `userCompany_unique` (`userId`,`companyId`),
-  KEY `fk_companymember_companyId_idx` (`companyId`),
-  KEY `fk_company_member_userId_idx` (`userId`),
-  CONSTRAINT `fk_companymember_companyId` FOREIGN KEY (`companyId`) REFERENCES `Company` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_company_member_userId` FOREIGN KEY (`userId`) REFERENCES `User` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
-)；
+  UNIQUE KEY `userId_companyId_UNIQUE` (`userId`,`companyId`),
+  KEY `fk_CompanyMember_companyId_idx` (`companyId`),
+  KEY `fk_CompanyMember_userId_idx` (`userId`),
+  CONSTRAINT `fk_CompanyMember_companyId` FOREIGN KEY (`companyId`) REFERENCES `Company` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_CompanyMember_userId` FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
