c242f7d2e7d004e046bc349d18cb5506feccb886
Leo Xu <leox@gobeta.com.cn>
2015-07-08 23:49:22 +0800

    re #11244, check for order conflict when creating orders

--- src/Sandbox/ApiBundle/Repository/Order/OrderRepository.php
+++ src/Sandbox/ApiBundle/Repository/Order/OrderRepository.php
@@ -6,4 +6,25 @@ use Doctrine\ORM\EntityRepository;
 
 class OrderRepository extends EntityRepository
 {
+    public function checkProductForClient(
+        $productId,
+        $startDate,
+        $endDate
+    ) {
+        $query = $this->createQueryBuilder('o')
+            ->Where('o.productId = :productId')
+            ->andWhere('o.status <> \'cancelled\'')
+            ->andWhere(
+                '(
+                    (o.startDate <= :startDate AND o.endDate > :startDate) OR
+                    (o.startDate < :endDate AND o.endDate >= :endDate)
+                )'
+            )
+            ->setParameter('productId', $productId)
+            ->setParameter('startDate', $startDate)
+            ->setParameter('endDate', $endDate)
+            ->getQuery();
+
+        return $query->getResult();
+    }
 }
