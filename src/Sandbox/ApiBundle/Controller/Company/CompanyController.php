0fdeae3fd91fa2281dc25d595c1745d60cc8fabe
Albert FENG <albert.feng@easylinks.com.cn>
2015-07-16 18:21:15 +0800

    re #add company portfolio api

--- src/Sandbox/ApiBundle/Controller/Company/CompanyController.php
+++ src/Sandbox/ApiBundle/Controller/Company/CompanyController.php
@@ -3,8 +3,6 @@
 namespace Sandbox\ApiBundle\Controller\Company;
 
 use Sandbox\ApiBundle\Controller\SandboxRestController;
-use Sandbox\ApiBundle\Entity\Company\CompanyPortfolio;
-use Sandbox\ApiBundle\Form\Company\CompanyPortfolioType;
 
 /**
  * Company Controller.
@@ -18,17 +16,4 @@ use Sandbox\ApiBundle\Form\Company\CompanyPortfolioType;
  */
 class CompanyController extends SandboxRestController
 {
-    protected function generateCompanyPortfolio(
-        $company,
-        $portfolio
-    ) {
-        $companyPortfolio = new CompanyPortfolio();
-
-        $form = $this->createForm(new CompanyPortfolioType(), $companyPortfolio);
-        $form->submit($portfolio);
-
-        $companyPortfolio->setCompany($company);
-
-        return $companyPortfolio;
-    }
 }
---
+++ src/Sandbox/ApiBundle/Controller/Company/CompanyPortfolioController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sandbox\ApiBundle\Controller\Company;
+
+use Sandbox\ApiBundle\Controller\SandboxRestController;
+use Sandbox\ApiBundle\Entity\Company\CompanyPortfolio;
+use Sandbox\ApiBundle\Form\Company\CompanyPortfolioType;
+
+/**
+ * Company Industry Controller.
+ *
+ * @category Sandbox
+ *
+ * @author   Albert Feng <albert.feng@easylinks.com.cn>
+ * @license  http://www.Sandbox.cn/ Proprietary
+ *
+ * @link     http://www.Sandbox.cn/
+ */
+class CompanyPortfolioController extends SandboxRestController
+{
+    /**
+     * @param $company
+     * @param $portfolio
+     */
+    protected function generateCompanyPortfolio(
+        $company,
+        $portfolio
+    ) {
+        $companyPortfolio = new CompanyPortfolio();
+
+        $form = $this->createForm(new CompanyPortfolioType(), $companyPortfolio);
+        $form->submit($portfolio);
+
+        $time = new \DateTime('now');
+        $companyPortfolio->setCreationDate($time);
+        $companyPortfolio->setModificationDate($time);
+        $companyPortfolio->setCompany($company);
+
+        return $companyPortfolio;
+    }
+}
