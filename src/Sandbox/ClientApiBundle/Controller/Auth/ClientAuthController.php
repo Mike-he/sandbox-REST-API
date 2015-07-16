47c7789a51dc6c347187eb4e64316086605c6466
Yimo Zhang <yimozh@gobeta.com.cn>
2015-07-16 20:24:41 +0800

    re #11434, client token auth api

---
+++ src/Sandbox/ClientApiBundle/Controller/Auth/ClientAuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Sandbox\ClientApiBundle\Controller\Auth;
+
+use Sandbox\ApiBundle\Controller\User\UserLoginController;
+use Sandbox\ApiBundle\Entity\User\User;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+/**
+ * Login controller.
+ *
+ * @category Sandbox
+ *
+ * @author   Yimo Zhang <yimo.zhang@Sandbox.cn>
+ * @license  http://www.Sandbox.cn/ Proprietary
+ *
+ * @link     http://www.Sandbox.cn/
+ */
+class ClientAuthController extends UserLoginController
+{
+    /**
+     * Token auth.
+     *
+     * @param Request $request the request object
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *  }
+     * )
+     *
+     * @Route("/me")
+     * @Method({"GET"})
+     *
+     * @return View
+     *
+     * @throws \Exception
+     */
+    public function getClientAuthMeAction(
+        Request $request
+    ) {
+        $myUserId = $this->getUserId();
+        $myUser = $this->getRepo('User\User')->find($myUserId);
+
+        if ($myUser->isBanned()) {
+            throw new UnauthorizedHttpException(null);
+        }
+
+        $view = new View();
+        $view->setData(array(
+            'id' => $myUserId,
+        ));
+
+        return $view;
+    }
+}
