2ff7c3c1632b5580562ae43d8dab149bec47450b
Leo Xu <leox@gobeta.com.cn>
2015-07-17 12:20:28 +0800

    re #11442, fix order price and remove card permission

--- src/Sandbox/AdminApiBundle/Controller/Door/AdminDoorController.php
+++ src/Sandbox/AdminApiBundle/Controller/Door/AdminDoorController.php
@@ -7,6 +7,7 @@ use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Delete;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 
@@ -25,12 +26,15 @@ class AdminDoorController extends DoorController
     const RESULT_OK = 'RESULT_OK';
     const RESPONSE_NOT_VALID_CODE = 400005;
     const RESPONSE_NOT_VALID_MESSAGE = 'Response Not Valid';
+    const TIME_NOT_VALID_CODE = 400006;
+    const TIME_NOT_VALID_MESSAGE = 'Times Are Not Valid';
     const LOGIN_URI = 'http://211.95.45.26:13390/ADSWebService.asmx/Login?Username=admin&Password=admin';
     const LOGOUT_URI = 'http://211.95.45.26:13390/ADSWebService.asmx/Logout?SessionId=';
     const GET_DOOR_URI = 'http://211.95.45.26:13390/ADSWebService.asmx/GetDoors?SessionId=';
     const SET_TIME = 'http://211.95.45.26:13390/ADSWebService.asmx/SetTimePeriod?SessionId=';
     const RECORD_URI = 'http://211.95.45.26:13390/ADSWebService.asmx/GetSwipeCardRecord?SessionId=';
     const ALARM_URI = 'http://211.95.45.26:13390/ADSWebService.asmx/GetAlarmRecord?SessionId=';
+    const SET_PERMISSION = 'http://211.95.45.26:13390/ADSWebService.asmx/SetCardPermission?SessionId=';
     const BEGIN_TIME = '&BeginTime=';
     const END_TIME = '&EndTime=';
 
@@ -67,21 +71,17 @@ class AdminDoorController extends DoorController
                 'ads_timeperiod' => [
                     'id' => '1',
                     'name' => 'time1',
-                    'begindate' => '2015-07-01',
-                    'enddate' => '2016-07-01',
+                    'begindate' => '2000-07-01',
+                    'enddate' => '2099-07-01',
                     'Mon' => '1',
                     'Tues' => '1',
                     'Weds' => '1',
                     'Thurs' => '1',
                     'Fri' => '1',
-                    'Sat' => '0',
-                    'Sun' => '0',
+                    'Sat' => '1',
+                    'Sun' => '1',
                     'times' => [
-                            ['begin' => '00:00', 'end' => '24:00'],
-                            ['begin' => '00:00', 'end' => '24:00'],
-                            ['begin' => '00:00', 'end' => '24:00'],
-                            ['begin' => '00:00', 'end' => '24:00'],
-                            ['begin' => '00:00', 'end' => '24:00'],
+                            ['begin' => '00:00:00', 'end' => '23:59:59'],
                         ],
                 ],
             ];
@@ -97,15 +97,101 @@ class AdminDoorController extends DoorController
                 );
             }
         } catch (\Exception $e) {
-            if (!is_null($sessionId) || !empty($sessionId)) {
+            if (!is_null($sessionId) && !empty($sessionId)) {
                 $this->logOut($sessionId);
             }
         }
         //TODO: store to database
     }
 
-    public function setCardPermissionAction()
-    {
+    /**
+     * @Post("/doors/permission/add")
+     *
+     * @param Request $request
+     *
+     * @return View
+     */
+    public function setCardPermissionAction(
+        Request $request
+    ) {
+        $sessionId = $this->getSessionId();
+        try {
+            $data = [
+                'ads_card' => [
+                    'empid' => '123456', //from user account
+                    'empname' => 'Jack', //from user account
+                    'department' => 'Sandhill',
+                    'cardno' => '1568208', //from user account
+                    'begindate' => '2015-07-16 08:00:00',
+                    'expiredate' => '2015-09-01 18:00:00',
+                    'operation' => 'add',
+                ],
+                'ads_door_permissions' => [
+                    ['doorid' => '{4B169885-76B7-4215-B3F3-318553AC0087}', 'timeperiodid' => '1'],
+                ],
+            ];
+            $data = json_encode($data);
+            //var_dump($data); exit;
+            $periodArray = $this->getDoorApi(self::SET_PERMISSION.$sessionId.'&CardPermission='.$data);
+            $this->logOut($sessionId);
+
+            if ($periodArray['ads_result']['result'] !== self::RESULT_OK) {
+                return $this->customErrorView(
+                    400,
+                    self::RESPONSE_NOT_VALID_CODE,
+                    self::RESPONSE_NOT_VALID_MESSAGE
+                );
+            }
+        } catch (\Exception $e) {
+            if (!is_null($sessionId) && !empty($sessionId)) {
+                $this->logOut($sessionId);
+            }
+        }
+    }
+
+    /**
+     * @Delete("/doors/permission/remove")
+     *
+     * @param Request $request
+     *
+     * @return View
+     */
+    public function removeCardPermissionAction(
+        Request $request
+    ) {
+        $sessionId = $this->getSessionId();
+        try {
+            $data = [
+                'ads_card' => [
+                    'empid' => '123456', //from user account
+                    'empname' => 'Jack', //from user account
+                    'department' => 'Sandhill',
+                    'cardno' => '1568208', //from user account
+                    'begindate' => '2015-07-16 08:00:00',
+                    'expiredate' => '2015-09-01 18:00:00',
+                    'operation' => 'delete',
+                ],
+                'ads_door_permissions' => [
+                    ['doorid' => '{4B169885-76B7-4215-B3F3-318553AC0087}', 'timeperiodid' => '1'],
+                ],
+            ];
+            $data = json_encode($data);
+
+            $periodArray = $this->getDoorApi(self::SET_PERMISSION.$sessionId.'&CardPermission='.$data);
+            $this->logOut($sessionId);
+
+            if ($periodArray['ads_result']['result'] !== self::RESULT_OK) {
+                return $this->customErrorView(
+                    400,
+                    self::RESPONSE_NOT_VALID_CODE,
+                    self::RESPONSE_NOT_VALID_MESSAGE
+                );
+            }
+        } catch (\Exception $e) {
+            if (!is_null($sessionId) && !empty($sessionId)) {
+                $this->logOut($sessionId);
+            }
+        }
     }
 
     /**
@@ -132,7 +218,7 @@ class AdminDoorController extends DoorController
 
             return new View($doorArray['ads_doors']);
         } catch (\Exception $e) {
-            if (!is_null($sessionId) || !empty($sessionId)) {
+            if (!is_null($sessionId) && !empty($sessionId)) {
                 $this->logOut($sessionId);
             }
         }
@@ -172,6 +258,13 @@ class AdminDoorController extends DoorController
         try {
             $begin = $paramFetcher->get('begin_time');
             $end = $paramFetcher->get('end_time');
+            if (is_null($begin) || is_null($end)) {
+                return $this->customErrorView(
+                    400,
+                    self::TIME_NOT_VALID_CODE,
+                    self::TIME_NOT_VALID_MESSAGE
+                );
+            }
 
             $url = self::RECORD_URI.$sessionId.self::BEGIN_TIME.$begin.self::END_TIME.$end;
             $url = urlencode($url);
@@ -189,7 +282,7 @@ class AdminDoorController extends DoorController
 
             return new View($recordArray['ads_swipecard_records']);
         } catch (\Exception $e) {
-            if (!is_null($sessionId) || !empty($sessionId)) {
+            if (!is_null($sessionId) && !empty($sessionId)) {
                 $this->logOut($sessionId);
             }
         }
@@ -229,6 +322,13 @@ class AdminDoorController extends DoorController
         try {
             $begin = $paramFetcher->get('begin_time');
             $end = $paramFetcher->get('end_time');
+            if (is_null($begin) || is_null($end)) {
+                return $this->customErrorView(
+                    400,
+                    self::TIME_NOT_VALID_CODE,
+                    self::TIME_NOT_VALID_MESSAGE
+                );
+            }
 
             $url = self::ALARM_URI.$sessionId.self::BEGIN_TIME.$begin.self::END_TIME.$end;
             $url = urlencode($url);
@@ -246,7 +346,7 @@ class AdminDoorController extends DoorController
 
             return new View($recordArray['ads_alarm_records']);
         } catch (\Exception $e) {
-            if (!is_null($sessionId) || !empty($sessionId)) {
+            if (!is_null($sessionId) && !empty($sessionId)) {
                 $this->logOut($sessionId);
             }
         }
