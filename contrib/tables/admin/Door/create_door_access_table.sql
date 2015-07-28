c2c00871dd88f7d725626c1738084e0264dd10f7
Leo Xu <leox@gobeta.com.cn>
2015-07-28 18:55:47 +0800

    re #11626, check if order is renew order

--- contrib/tables/admin/Door/create_door_access_table.sql
+++ contrib/tables/admin/Door/create_door_access_table.sql
@@ -2,8 +2,11 @@ CREATE TABLE `DoorAccess` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `buildingId` int(11) NOT NULL,
+  `roomId` int(11) NOT NULL,
   `doorId` varchar(64) NOT NULL,
   `timeId` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `startDate` datetime NOT NULL,
   `endDate` datetime NOT NULL,
   `creationDate` datetime NOT NULL,
   PRIMARY KEY (`id`)
