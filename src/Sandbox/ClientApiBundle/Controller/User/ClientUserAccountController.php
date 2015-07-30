d94bf0c9613d9b7fb5a0de1221a3461640881a69
Mike He <mike.he@easylinks.com.cn>
2015-07-30 14:18:08 +0800

    re #11732,get user account

---
+++ src/Sandbox/ClientApiBundle/Controller/User/ClientUserAccountController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sandbox\ClientApiBundle\Controller\User;
+
+use Sandbox\ApiBundle\Controller\SandboxRestController;
+use Sandbox\ApiBundle\Entity\User\User;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+/**
+ * User Account controller.
+ *
+ * @category Sandbox
+ *
+ * @author   Mike He <mike.he@easylinks.com.cn>
+ * @license  http://www.Sandbox.cn/ Proprietary
+ *
+ * @link     http://www.Sandbox.cn/
+ */
+class ClientUserAccountController extends SandboxRestController
+{
+    /**
+     * Get my user account.
+     *
+     * @param Request $request the request object
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *  }
+     * )
+     *
+     * @Method({"GET"})
+     * @Route("/account")
+     *
+     * @return View
+     *
+     * @throws \Exception
+     */
+    public function getUserAccountAction(
+        Request $request
+    ) {
+        $userId = $this->getUserId();
+        $user = $this->getRepo('User\User')->find($userId);
+        $view = new View();
+        $view->setData(
+            array(
+                'id' => $userId,
+                'email' => $user->getEmail(),
+                'phone' => $user->getPhone(),
+            )
+        );
+
+        return $view;
+    }
+}
