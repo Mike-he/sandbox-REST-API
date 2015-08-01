a9cc3eb65f164624c6a1f7d759931c7a0d652cbe
Leo Xu <leox@gobeta.com.cn>
2015-08-01 16:43:04 +0800

    re #11835, update product search for meeting room

--- src/Sandbox/ApiBundle/Repository/Product/ProductRepository.php
+++ src/Sandbox/ApiBundle/Repository/Product/ProductRepository.php
@@ -8,6 +8,110 @@ use Sandbox\ApiBundle\Entity\Room\RoomCity;
 
 class ProductRepository extends EntityRepository
 {
+    public function getMeetingProductsNotInOrder(
+
+    ) {
+    }
+
+    /**
+     * @param $userId
+     * @param $cityId
+     * @param $buildingId
+     * @param $allowedPeople
+     * @param $startDate
+     * @param $endDate
+     * @param $limit
+     * @param $offset
+     *
+     * @return array
+     */
+    public function getMeetingProductsForClient(
+        $userId,
+        $cityId,
+        $buildingId,
+        $allowedPeople,
+        $startTime,
+        $endTime,
+        $startHour,
+        $endHour,
+        $limit,
+        $offset
+    ) {
+        $query = $this->createQueryBuilder('p')
+            ->select('DISTINCT p.id')
+            ->leftjoin('SandboxApiBundle:Room\Room', 'r', 'WITH', 'r.id = p.roomId')
+            ->leftJoin('SandboxApiBundle:Room\RoomMeeting', 'm', 'WITH', 'p.roomId = m.room')
+            ->where('p.visibleUserId = :userId OR p.private = :private')
+            ->andWhere('r.type = \'meeting\'')
+            ->setParameter('private', false)
+            ->setParameter('userId', $userId);
+        if (!is_null($cityId)) {
+            $query = $query->andWhere('r.city = :cityId')
+                ->setParameter('cityId', $cityId);
+        }
+        if (!is_null($buildingId)) {
+            $query = $query->andWhere('r.building = :buildingId')
+                ->setParameter('buildingId', $buildingId);
+        }
+        if (!is_null($allowedPeople)) {
+            $query = $query->andWhere('r.allowedPeople >= :allowedPeople')
+                ->setParameter('allowedPeople', $allowedPeople);
+        }
+        if (!is_null($startTime) && is_null($endTime)) {
+            $query = $query
+                //->select('sum(hour(DATE_DIFF(o.startDate, o.endDate))) as sum')
+                ->select(
+                    '
+                        p.id,
+                        SUM(hour(DATE_DIFF(o.startDate, o.endDate))) as diff,
+                        hour(DATE_DIFF(m.startHour, m.endHour)) as total
+                    '
+                )
+                ->leftJoin('SandboxApiBundle:Order\ProductOrder', 'o', 'WITH', 'o.productId = p.id')
+                ->andWhere('m.startHour <= :startHour AND m.endHour > :startHour')
+                ->andWhere('p.startDate <= :startTime AND p.endDate > :startTime')
+//                ->andWhere(
+//                    'p.id IN (
+//                        SELECT po.productId AND sum(hour(DATE_DIFF(po.startDate, po.endDate))) as diffSum
+//                        WHERE diffSum > 1
+//                        FROM SandboxApiBundle:Order\ProductOrder po
+//                        GROUP BY po.productId
+//                    )'
+//                )
+//                        ->orWhere('o.endDate <= :startTime')
+//                ->orWhere('o.startDate > :startTime')
+                ->groupBy('o.productId')
+                ->having('diff < total')
+                ->setParameter('startTime', $startTime)
+                ->setParameter('startHour', $startHour);
+        }
+        if (!is_null($startTime) && !is_null($endTime)) {
+            $query = $query->andWhere('m.startHour <= :startHour AND m.endHour >= :endHour')
+                ->andWhere('p.startDate <= :startTime')
+                ->andWhere('p.endDate >= :endTime')
+                ->andWhere(
+                    'p.id NOT IN (
+                        SELECT po.productId FROM SandboxApiBundle:Order\ProductOrder po
+                        WHERE po.status <> \'cancelled\' AND
+                        (
+                            (po.startDate <= :startTime AND po.endDate > :startTime) OR
+                            (po.startDate < :endTime AND po.endDate >= :endTime)
+                        )
+                    )'
+                )
+                ->setParameter('startTime', $startTime)
+                ->setParameter('endTime', $endTime)
+                ->setParameter('startHour', $startHour)
+                ->setParameter('endHour', $endHour);
+        }
+        $query = $query->orderBy('p.creationDate', 'DESC')
+            ->setMaxResults($limit)
+            ->setFirstResult($offset)
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
     /**
      * @param $userId
      * @param $cityId
