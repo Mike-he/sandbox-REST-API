7e5254b7f6356da3e37918db87194e78e75eea0f
Mike He <mike.he@easylinks.com.cn>
2015-10-15 13:57:04 +0800

    close #13300, count feed comments when user authorized

--- app/DoctrineMigrations/Version20151015094003_bug_13300.php
+++ app/DoctrineMigrations/Version20151015094003_bug_13300.php
@@ -21,8 +21,8 @@ class Version20151015094003_bug_13300 extends AbstractMigration
         $this->addSql('DROP VIEW FeedView');
         $this->addSql('CREATE VIEW FeedView AS
                         SELECT DISTINCT f.*,
-                               (SELECT COUNT(fc.id) FROM FeedComment fc LEFT JOIN User u1 ON u1.id = fc.authorId WHERE fc.feedId = f.id AND u1.banned = FALSE) AS comments_count,
-                               (SELECT COUNT(fl.id) FROM FeedLike fl LEFT JOIN User u2 ON u2.id = fl.authorId WHERE fl.feedId = f.id AND u2.banned = FALSE) AS likes_count
+                               (SELECT COUNT(fc.id) FROM FeedComment fc LEFT JOIN User u1 ON u1.id = fc.authorId WHERE fc.feedId = f.id AND u1.banned = FALSE AND u1.authorized = TRUE ) AS comments_count,
+                               (SELECT COUNT(fl.id) FROM FeedLike fl LEFT JOIN User u2 ON u2.id = fl.authorId WHERE fl.feedId = f.id AND u2.banned = FALSE AND u2.authorized = TRUE ) AS likes_count
                         FROM Feed f
                         LEFT JOIN User u ON u.id = f.ownerId
                         WHERE u.banned = FALSE
@@ -39,8 +39,8 @@ class Version20151015094003_bug_13300 extends AbstractMigration
 
         $this->addSql('CREATE VIEW FeedView AS
                         SELECT DISTINCT f.*,
-                               (SELECT COUNT(fc.id) FROM FeedComment fc LEFT JOIN User u1 ON u1.id = fc.authorId WHERE fc.feedId = f.id AND u1.banned = FALSE) AS comments_count,
-                               (SELECT COUNT(fl.id) FROM FeedLike fl LEFT JOIN User u2 ON u2.id = fl.authorId WHERE fl.feedId = f.id AND u2.banned = FALSE) AS likes_count
+                               (SELECT COUNT(fc.id) FROM FeedComment fc LEFT JOIN User u1 ON u1.id = fc.authorId WHERE fc.feedId = f.id AND u1.banned = FALSE AND u1.authorized = TRUE ) AS comments_count,
+                               (SELECT COUNT(fl.id) FROM FeedLike fl LEFT JOIN User u2 ON u2.id = fl.authorId WHERE fl.feedId = f.id AND u2.banned = FALSE AND u2.authorized = TRUE ) AS likes_count
                         FROM Feed f
                         LEFT JOIN User u ON u.id = f.ownerId
                         WHERE u.banned = FALSE
