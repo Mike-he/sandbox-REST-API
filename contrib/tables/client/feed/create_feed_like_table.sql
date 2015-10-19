c04903e0a7e16ee8aa4f85abc22db2f0ff2110ad
Yimo Zhang <yimozh@gobeta.com.cn>
2015-10-19 10:49:55 +0800

    update FeedLike table

--- contrib/tables/client/feed/create_feed_like_table.sql
+++ contrib/tables/client/feed/create_feed_like_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `FeedLike` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `feedId` int(11) NOT NULL,
-  `authorId` varchar(64) NOT NULL,
+  `authorId` int(11) NOT NULL,
   `creationDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `feed_like_feedId_authorId_UNIQUE` (`feedId`,`authorId`),
-  KEY `feed_like_fk_feedId_idx` (`feedId`),
-  KEY `fk_feedlike_authorId_idx` (`authorId`),
-  CONSTRAINT `fk_feedLike_feedId` FOREIGN KEY (`feedId`) REFERENCES `Feed` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_feedlike_authorId` FOREIGN KEY (`authorId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  UNIQUE KEY `feedId_authorId_UNIQUE` (`feedId`,`authorId`),
+  KEY `fk_FeedLike_feedId_idx` (`feedId`),
+  KEY `fk_FeedLike_authorId_idx` (`authorId`),
+  CONSTRAINT `fk_FeedLike_feedId` FOREIGN KEY (`feedId`) REFERENCES `Feed` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_FeedLike_authorId` FOREIGN KEY (`authorId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 );
