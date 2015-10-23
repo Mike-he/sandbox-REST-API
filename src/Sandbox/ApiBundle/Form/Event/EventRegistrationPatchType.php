82dbc2e92c2262bba8558a2821426957217c0be3
Mike He <mike.he@easylinks.com.cn>
2015-10-23 14:25:38 +0800

    close #13399, patch event registration

---
+++ src/Sandbox/ApiBundle/Form/Event/EventRegistrationPatchType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sandbox\ApiBundle\Form\Event;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventRegistrationPatchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('status')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sandbox\ApiBundle\Entity\Event\EventRegistration',
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return '';
+    }
+}
