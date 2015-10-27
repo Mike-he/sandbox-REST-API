259cb8c064e3354142e66d7c590de1a0718235d2
Yimo Zhang <yimozh@gobeta.com.cn>
2015-10-27 15:45:58 +0800

    fix insert query

--- contrib/tables/client/app/create_app_info_table.sql
+++ contrib/tables/client/app/create_app_info_table.sql
@@ -7,5 +7,5 @@ CREATE TABLE `AppInfo` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO AdminType(`platform`,`version`,`url`,`date`) VALUES('ios','1.0','http://','2015-10-10');
-INSERT INTO AdminType(`platform`,`version`,`url`,`date`) VALUES('android','1.0','http://','2015-10-10');
+INSERT INTO AppInfo(`platform`,`version`,`url`,`date`) VALUES('ios','1.0','itms-services://?action=download-manifest&url=https://gitcafe.com/kiddx/sandbox/raw/master/production.plist','2015-10-10');
+INSERT INTO AppInfo(`platform`,`version`,`url`,`date`) VALUES('android','1.0','http://www.gobeta.com.cn/sandbox/SandBox_pro.apk','2015-10-10');
