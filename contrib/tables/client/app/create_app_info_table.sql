30b22087241d5a498b6a6a2671700d2528302c12
Yimo Zhang <yimozh@gobeta.com.cn>
2015-10-27 16:04:18 +0800

    change primary key to platform

--- contrib/tables/client/app/create_app_info_table.sql
+++ contrib/tables/client/app/create_app_info_table.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `AppInfo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `platform` varchar(16) NOT NULL,
   `version` varchar(16) NOT NULL,
   `url` varchar(128) NOT NULL,
   `date` varchar(16) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`platform`)
 );
 
 INSERT INTO AppInfo(`platform`,`version`,`url`,`date`) VALUES('ios','1.0','itms-services://?action=download-manifest&url=https://gitcafe.com/kiddx/sandbox/raw/master/production.plist','2015-10-10');
