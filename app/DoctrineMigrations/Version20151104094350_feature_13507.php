4a1bb336ab378ef8f9836ae54b58aac60c20f5e2
Mike He <mike.he@easylinks.com.cn>
2015-11-05 15:31:05 +0800

    close #13507, modify attachment name

--- app/DoctrineMigrations/Version20151104094350_feature_13507.php
+++ app/DoctrineMigrations/Version20151104094350_feature_13507.php
@@ -20,7 +20,7 @@ class Version20151104094350_feature_13507 extends AbstractMigration
 
         $this->addSql('CREATE TABLE News (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(1024) NOT NULL, description LONGTEXT NOT NULL, visible TINYINT(1) NOT NULL, creationDate DATETIME NOT NULL, modificationDate DATETIME NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE NewsAttachment (id INT AUTO_INCREMENT NOT NULL, newsId INT NOT NULL, content LONGTEXT NOT NULL, attachmentType VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL, preview LONGTEXT NOT NULL, size INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE NewsAttachment ADD CONSTRAINT fk_NewsAttachments_newsId FOREIGN KEY (newsId) REFERENCES News(id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE NewsAttachment ADD CONSTRAINT fk_NewsAttachment_newsId FOREIGN KEY (newsId) REFERENCES News(id) ON DELETE CASCADE');
         $this->addSql("INSERT INTO AdminPermission(`typeId`,`key`,`name`,`creationDate`,`modificationDate`) VALUES(2, 'platform.news', '新闻管理', '2015-08-24 00:00:00', '2015-08-24 00:00:00')");
     }
 
@@ -33,7 +33,7 @@ class Version20151104094350_feature_13507 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE News');
-        $this->addSql('DROP TABLE NewsAttachments');
-        $this->addSql('ALTER TABLE NewsAttachments DROP fk_NewsAttachments_newsId');
+        $this->addSql('DROP TABLE NewsAttachment');
+        $this->addSql('ALTER TABLE NewsAttachment DROP fk_NewsAttachments_newsId');
     }
 }
