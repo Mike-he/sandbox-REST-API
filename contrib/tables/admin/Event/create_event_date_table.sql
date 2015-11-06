b51d5c87e0f8098b633ad7493cdc9a034a1f7a57
Mike He <mike.he@easylinks.com.cn>
2015-11-06 18:19:38 +0800

    close #13492, fix event date contrib

--- contrib/tables/admin/Event/create_event_date_table.sql
+++ contrib/tables/admin/Event/create_event_date_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `EventDate` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `eventId` int(11) NOT NULL,
-  `date` datetime NOT NULL,
+  `date` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_EventDate_eventId_idx` (`eventId`),
   CONSTRAINT `fk_EventDate_eventId` FOREIGN KEY (`eventId`) REFERENCES `Event` (`id`) ON DELETE CASCADE
