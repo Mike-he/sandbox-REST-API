16ce406372650af683274980473a3b18672edca3
Mike He <mike.he@easylinks.com.cn>
2015-11-16 18:09:15 +0800

    close #13642, modify contrib sql

--- contrib/tables/client/feed/create_feed_table.sql
+++ contrib/tables/client/feed/create_feed_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE `Feed` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text CHARACTER SET utf8mb4 NOT NULL,
   `ownerId` varchar(64) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
   `creationDate` datetime NOT NULL,
   PRIMARY KEY (`id`)
 );
