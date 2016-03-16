8a9a503de90db243b6fc75555e68d2de9dd34173
yimo24 <yimozh@gobeta.com.cn>
2016-03-16 11:51:35 +0800

    close #14889, package wechat api to retrieve sns user info

---
+++ src/Sandbox/ClientApiBundle/Controller/ThirdParty/ClientThirdPartyWeChatController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Sandbox\ClientApiBundle\Controller\ThirdParty;
+
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sandbox\ApiBundle\Entity\ThirdParty\WeChat;
+use Sandbox\ApiBundle\Traits\WeChatApi;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Client Third Party WeChat controller.
+ *
+ * @category Sandbox
+ *
+ * @author   Yimo Zhang <yimo.zhang@Sandbox.cn>
+ * @license  http://www.Sandbox.cn/ Proprietary
+ *
+ * @link     http://www.Sandbox.cn/
+ */
+class ClientThirdPartyWeChatController extends ClientThirdPartyController
+{
+    use WeChatApi;
+
+    /**
+     * Retrieve WeChat user info.
+     *
+     * @param Request $request
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *  }
+     * )
+     *
+     * @Route("/sns/userinfo")
+     * @Method({"GET"})
+     *
+     * @throws \Exception
+     *
+     * @return View
+     */
+    public function getWeChatSnsUserInfoAction(
+        Request $request
+    ) {
+        $userId = $this->getUserId();
+        $user = $this->getRepo('User\User')->find($userId);
+
+        // get WeChat bind to me
+        $weChat = $this->getRepo('ThirdParty\WeChat')->findOneByUser($user);
+        if (is_null($weChat)) {
+            $this->throwNotFoundIfNull($weChat, self::NOT_FOUND_MESSAGE);
+        }
+
+        $result = $this->getWeChatSnsUserInfo($weChat);
+
+        return new View($result);
+    }
+}
