009cd718115c0d8f7bed5defdb5418aa54615573
Mike He <mike.he@easylinks.com.cn>
2016-04-08 15:39:26 +0800

    close #15296, fix recover production visible bug

--- src/Sandbox/ApiBundle/Repository/Product/ProductRepository.php
+++ src/Sandbox/ApiBundle/Repository/Product/ProductRepository.php
@@ -527,6 +527,35 @@ class ProductRepository extends EntityRepository
     }
 
     /**
+     * @param $building
+     */
+    public function setVisibleTrue(
+        $building
+    ) {
+        $now = new \DateTime();
+        $nowString = (string) $now->format('Y-m-d H:i:s');
+        $nowString = "'$nowString'";
+        $valueFalse = 'true';
+
+        $query = $this->createQueryBuilder('p')
+            ->leftJoin('SandboxApiBundle:Room\Room', 'r', 'WITH', 'r.id = p.roomId')
+            ->where('r.building = :building')
+            ->update()
+            ->set('p.visible', $valueFalse)
+            ->set('p.modificationDate', $nowString)
+            ->where('p.visible = :status')
+            ->andWhere('p.isDeleted = FALSE')
+            ->andWhere('p.startDate <= :now')
+            ->andWhere('p.endDate > :now')
+            ->setParameter('now', $now)
+            ->setParameter('status', false)
+            ->setParameter('building', $building)
+            ->getQuery();
+
+        $query->execute();
+    }
+
+    /**
      * @param Product      $product
      * @param string       $action
      * @param RoomCity     $city
