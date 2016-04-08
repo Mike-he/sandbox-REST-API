1530e47bd6c6203c4c08645906ceff250dc1f356
Leo Xu <leox@gobeta.com.cn>
2016-04-08 15:53:38 +0800

    close #15297, add table

--- contrib/tables/admin/Sales/create_sales_user_table.sql
+++ contrib/tables/admin/Sales/create_sales_user_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `SalesUser` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
+  `shopId` int(11),
   `companyId` int(11) NOT NULL,
   `buildingId` int(11) NOT NULL,
   `isOrdered` tinyint(1) NOT NULL,
+  `isShopOrdered` tinyint(1) NOT NULL,
   `isAuthorized` tinyint(1) NOT NULL,
   `creationDate` datetime NOT NULL,
   `modificationDate` datetime NOT NULL,
