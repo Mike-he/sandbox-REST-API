ad424dc325f838d39f9a758f4e1530c4ade5ab91
Leo Xu <leox@gobeta.com.cn>
2016-04-12 14:30:54 +0800

    close #15303, shop order export

---
+++ src/Sandbox/ApiBundle/Constants/ShopOrderExport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sandbox\ApiBundle\Constants;
+
+class ShopOrderExport
+{
+    const ORDER_NUMBER = 'order_number';
+    const SHOP_NAME = 'shop';
+    const ORDER_TIME = 'order_time';
+    const PRODUCT_NAME = 'product_name';
+    const PRODUCT_TYPE = 'product_type';
+    const USER_NAME = 'user';
+    const TOTAL_AMOUNT = 'amount';
+    const TOTAL_PRICE = 'price';
+    const TOTAL_REFUND = 'refund';
+    const ORDER_STATUS = 'status';
+    const PAY_CHANNEL = 'channel';
+
+    const TRANS_ROOM_TYPE = 'room.type.';
+    const TRANS_ROOM_UNIT = 'room.unit.';
+
+    const TRANS_SHOP_ORDER_STATUS = 'shop_order.status.';
+    const TRANS_SHOP_ORDER_CHANNEL = 'shop_order.channel.';
+
+    const TRANS_SHOP_ORDER_HEADER_ORDER_NO = 'shop_order.export_header.order_number';
+    const TRANS_SHOP_ORDER_HEADER_SHOP = 'shop_order.export_header.shop';
+    const TRANS_SHOP_ORDER_HEADER_ORDER_TIME = 'shop_order.export_header.order_time';
+    const TRANS_SHOP_ORDER_HEADER_PRODUCT_NAME = 'shop_order.export_header.product_name';
+    const TRANS_SHOP_ORDER_HEADER_PRODUCT_TYPE = 'shop_order.export_header.product_type';
+    const TRANS_SHOP_ORDER_HEADER_USER = 'shop_order.export_header.user';
+    const TRANS_SHOP_ORDER_HEADER_AMOUNT = 'shop_order.export_header.amount';
+    const TRANS_SHOP_ORDER_HEADER_TOTAL_PRICE = 'shop_order.export_header.total_price';
+    const TRANS_SHOP_ORDER_HEADER_REFUND = 'shop_order.export_header.refund';
+    const TRANS_SHOP_ORDER_HEADER_STATUS = 'shop_order.export_header.status';
+    const TRANS_SHOP_ORDER_HEADER_CHANNEL = 'shop_order.export_header.channel';
+}
