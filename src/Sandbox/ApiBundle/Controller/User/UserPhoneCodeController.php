d18df50192a4b4ac7b1dff7b155e89841f3d6da0
Mike He <mike.he@easylinks.com.cn>
2016-05-19 11:41:59 +0800

    close #15823, phone code list

---
+++ src/Sandbox/ApiBundle/Controller/User/UserPhoneCodeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sandbox\ApiBundle\Controller\User;
+
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Sandbox\ApiBundle\Controller\SandboxRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use FOS\RestBundle\View\View;
+
+class UserPhoneCodeController extends SandboxRestController
+{
+    /**
+     * @param Request               $request
+     * @param ParamFetcherInterface $paramFetcher
+     *
+     * @Route("/phonecode/lists")
+     * @Method({"GET"})
+     *
+     * @return View
+     */
+    public function getPhoneCodeListAction(
+        Request $request,
+        ParamFetcherInterface $paramFetcher
+    ) {
+        $lists = $this->getRepo('User\UserPhoneCode')->findAll();
+
+        return new View($lists);
+    }
+}
