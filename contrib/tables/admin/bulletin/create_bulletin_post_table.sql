61095283837e8d801791a4e97ae36237b4164379
Leo Xu <leox@gobeta.com.cn>
2016-06-06 15:19:12 +0800

    close #16015, fix bulletin post contrib table

--- contrib/tables/admin/bulletin/create_bulletin_post_table.sql
+++ contrib/tables/admin/bulletin/create_bulletin_post_table.sql
@@ -2,11 +2,11 @@ CREATE TABLE `BulletinPost` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `typeId` int(11) NOT NULL,
   `title` varchar(128) NOT NULL,
-  `description` varchar(1024) NOT NULL,
-  `content` longtext NOT NULL,
+  `description` longtext NOT NULL,
   `deleted` tinyint(1) DEFAULT FALSE NOT NULL,
   `creationDate` datetime NOT NULL,
   `modificationDate` datetime NOT NULL,
+  `sortTime` varchar(15) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_BulletinPost_typeId_idx` (`typeId`),
   CONSTRAINT `fk_BulletinPost_typeId` FOREIGN KEY (`typeId`) REFERENCES `BulletinType` (`id`) ON DELETE CASCADE
