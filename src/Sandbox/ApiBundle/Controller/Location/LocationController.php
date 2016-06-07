45cb7eb4cb6cc78948b32fc68440eccc260ca1af
Leo Xu <leox@gobeta.com.cn>
2016-06-08 06:54:27 +0800

    fix bug

--- src/Sandbox/ApiBundle/Controller/Location/LocationController.php
+++ src/Sandbox/ApiBundle/Controller/Location/LocationController.php
@@ -200,74 +200,78 @@ class LocationController extends SalesRestController
             $visible
         );
 
-        $clientId = $this->getUser()->getClientId();
-        $client = $this->getRepo('User\UserClient')->find($clientId);
+        $user = $this->getUser();
 
-        if (!is_null($client)) {
-            $name = $client->getName();
+        if (!is_null($user)) {
+            $clientId = $user->getClientId();
+            $client = $this->getRepo('User\UserClient')->find($clientId);
 
-            if ($name == 'SandBox iOS') {
-                foreach ($buildings as $building) {
-                    switch ($building->getId()) {
-                        case 6:
-                            $building->setLat(31.210573);
-                            $building->setLng(121.626096);
+            if (!is_null($client)) {
+                $name = $client->getName();
 
-                            break;
-                        case 9:
-                            $building->setLat(31.2105);
-                            $building->setLng(121.625838);
+                if ($name == 'SandBox iOS') {
+                    foreach ($buildings as $building) {
+                        switch ($building->getId()) {
+                            case 6:
+                                $building->setLat(31.210573);
+                                $building->setLng(121.626096);
 
-                            break;
-                        case 10:
-                            $building->setLat(31.231719);
-                            $building->setLng(121.463842);
+                                break;
+                            case 9:
+                                $building->setLat(31.2105);
+                                $building->setLng(121.625838);
 
-                            break;
-                        case 11:
-                            $building->setLat(31.271693);
-                            $building->setLng(121.533756);
+                                break;
+                            case 10:
+                                $building->setLat(31.231719);
+                                $building->setLng(121.463842);
 
-                            break;
-                        case 20:
-                            $building->setLat(31.207687);
-                            $building->setLng(121.611939);
+                                break;
+                            case 11:
+                                $building->setLat(31.271693);
+                                $building->setLng(121.533756);
 
-                            break;
-                        default:
-                            break;
+                                break;
+                            case 20:
+                                $building->setLat(31.207687);
+                                $building->setLng(121.611939);
+
+                                break;
+                            default:
+                                break;
+                        }
                     }
-                }
-            } elseif ($name == 'Sandbox Android') {
-                foreach ($buildings as $building) {
-                    switch ($building->getId()) {
-                        case 6:
-                            $building->setLat(31.21616);
-                            $building->setLng(121.633312);
+                } elseif ($name == 'Sandbox Android') {
+                    foreach ($buildings as $building) {
+                        switch ($building->getId()) {
+                            case 6:
+                                $building->setLat(31.21616);
+                                $building->setLng(121.633312);
 
-                            break;
-                        case 9:
-                            $building->setLat(31.21606);
-                            $building->setLng(121.633212);
+                                break;
+                            case 9:
+                                $building->setLat(31.21606);
+                                $building->setLng(121.633212);
 
-                            break;
-                        case 10:
-                            $building->setLat(31.237882);
-                            $building->setLng(121.470016);
+                                break;
+                            case 10:
+                                $building->setLat(31.237882);
+                                $building->setLng(121.470016);
 
-                            break;
-                        case 11:
-                            $building->setLat(31.277719);
-                            $building->setLng(121.539895);
+                                break;
+                            case 11:
+                                $building->setLat(31.277719);
+                                $building->setLng(121.539895);
 
-                            break;
-                        case 20:
-                            $building->setLat(31.213244);
-                            $building->setLng(121.618609);
+                                break;
+                            case 20:
+                                $building->setLat(31.213244);
+                                $building->setLng(121.618609);
 
-                            break;
-                        default:
-                            break;
+                                break;
+                            default:
+                                break;
+                        }
                     }
                 }
             }
