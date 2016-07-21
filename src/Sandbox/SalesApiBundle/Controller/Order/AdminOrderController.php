1558870270a88adcf900ea3d9cc9e5f873bcd12e
Leo Xu <leox@gobeta.com.cn>
2016-07-21 14:21:13 +0800

    Revert "close #16379, fix admin order cancel"
    This reverts commit 9ba6705bbc9d33186814136b73205e72109959d0.

--- src/Sandbox/SalesApiBundle/Controller/Order/AdminOrderController.php
+++ src/Sandbox/SalesApiBundle/Controller/Order/AdminOrderController.php
@@ -969,9 +969,8 @@ class AdminOrderController extends OrderController
         }
 
         $order->setCancelByUser(true);
-        $payDate = $order->getPaymentDate();
 
-        if (ProductOrder::PREORDER_TYPE == $type && !is_null($payDate)) {
+        if (ProductOrder::PREORDER_TYPE == $type) {
             if (ProductOrder::STATUS_COMPLETED == $status) {
                 return $this->customErrorView(
                     400,
