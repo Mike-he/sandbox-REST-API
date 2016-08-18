b4e3b350e6e5eb2b1d53ba33c650edcf801ac14e
Mike He <mike.he@easylinks.com.cn>
2016-08-18 10:51:45 +0800

    update contrib

--- contrib/tables/client/thirdparty/create_wechat_share_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `WeChatShare` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `keyName` VARCHAR(122) NOT NULL,
-  `value` VARCHAR(1024) NOT NULL,
-  `creationDate` datetime NOT NULL,
-  `modificationDate` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-);
