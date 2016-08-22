9e5e9b6fe2f980265c876bad93d21bc0646c5899
feng.li <feng.li@sandbox3.cn>
2016-08-22 13:35:13 +0800

    db update

--- contrib/tables/admin/advertising/create_advertising_table.sql
+++ contrib/tables/admin/advertising/create_advertising_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `Advertising` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `url` longtext COLLATE utf8_unicode_ci NOT NULL,
   `source` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `sourceId` int(11) DEFAULT NULL,
   `visible` tinyint(1) NOT NULL,
