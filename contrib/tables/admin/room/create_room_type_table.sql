065e6fa91585b7c210b42c35d237a815a80f83fe
Mike He <mike.he@easylinks.com.cn>
2016-09-08 09:50:39 +0800

    fix contrib

--- contrib/tables/admin/room/create_room_type_table.sql
+++ contrib/tables/admin/room/create_room_type_table.sql
@@ -4,9 +4,9 @@ CREATE TABLE `RoomTypes` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `RoomTypes` (`name`, `icon`) VALUES('office', 'https://image.sandbox3.cn/icon/room_type_office.png');
-INSERT INTO `RoomTypes` (`name`, `icon`) VALUES('meeting', 'https://image.sandbox3.cn/icon/room_type_meeting.png');
-INSERT INTO `RoomTypes` (`name`, `icon`) VALUES('flexible', 'https://image.sandbox3.cn/icon/room_type_flexible.png');
-INSERT INTO `RoomTypes` (`name`, `icon`) VALUES('fixed', 'https://image.sandbox3.cn/icon/room_type_fixed.png');
-INSERT INTO `RoomTypes` (`name`, `icon`) VALUES('studio', 'https://image.sandbox3.cn/icon/room_type_studio.png');
-INSERT INTO `RoomTypes` (`name`, `icon`) VALUES('space', 'https://image.sandbox3.cn/icon/room_type_space.png');
+INSERT INTO `RoomTypes` (`key`, `icon`) VALUES('office', 'https://image.sandbox3.cn/icon/room_type_office.png');
+INSERT INTO `RoomTypes` (`key`, `icon`) VALUES('meeting', 'https://image.sandbox3.cn/icon/room_type_meeting.png');
+INSERT INTO `RoomTypes` (`key`, `icon`) VALUES('flexible', 'https://image.sandbox3.cn/icon/room_type_flexible.png');
+INSERT INTO `RoomTypes` (`key`, `icon`) VALUES('fixed', 'https://image.sandbox3.cn/icon/room_type_fixed.png');
+INSERT INTO `RoomTypes` (`key`, `icon`) VALUES('studio', 'https://image.sandbox3.cn/icon/room_type_studio.png');
+INSERT INTO `RoomTypes` (`key`, `icon`) VALUES('space', 'https://image.sandbox3.cn/icon/room_type_space.png');
