f20ebb5866f19b273761710eb95b8009b2c7fe06
Albert Feng <albert.feng@easylinks.com.cn>
2016-10-12 17:06:12 +0800

    close #refactor api admin auth me

--- src/Sandbox/ApiBundle/Repository/Admin/AdminPermissionRepository.php
+++ src/Sandbox/ApiBundle/Repository/Admin/AdminPermissionRepository.php
@@ -51,86 +51,30 @@ class AdminPermissionRepository extends EntityRepository
     }
 
     /**
-     * @param int    $admin
      * @param string $platform
      * @param int    $salesCompanyId
      *
      * @return array
      */
-    public function findAdminPermissionsByAdminAndPlatform(
-        $admin,
+    public function findSuperAdminPermissionsByPlatform(
         $platform,
         $salesCompanyId = null
     ) {
         $excludePermissionIds = $this->findAdminExcludePermissionIds($platform, $salesCompanyId);
 
-        $positions = $this->getEntityManager()
-        ->createQueryBuilder()
-            ->select('p.id')
-            ->from('SandboxApiBundle:Admin\AdminPositionUserBinding', 'b')
-            ->join('b.position', 'p')
-            ->where('b.userId = :admin')
-            ->andWhere('p.platform = :platform')
-            ->setParameter('platform', $platform)
-            ->setParameter('admin', $admin);
-
-        $clonePositions = clone $positions;
-
-        $superAdminPosition = $positions
-            ->andWhere('p.isSuperAdmin = 1')
-            ->getQuery()
-            ->getResult();
-
-        // if it's a super admin, return all permissions
-        if (!empty($superAdminPosition)) {
-            $permission = $this->getEntityManager()
-                ->createQueryBuilder()
-                ->select('ap.id, ap.name, ap.key, ap.maxOpLevel as op_level')
-                ->from('SandboxApiBundle:Admin\AdminPermission', 'ap')
-                ->where('ap.platform = :platform')
-                ->setParameter('platform', $platform);
-
-            if (!empty($excludePermissionIds)) {
-                $permission->andWhere('ap.id NOT IN (:excludePermissionIds)')
-                    ->setParameter('excludePermissionIds', $excludePermissionIds);
-            }
-
-            return $permission->getQuery()->getResult();
-        }
-
-        // if it's not a super admin
-        $clonePositions->andWhere('p.isSuperAdmin = 0');
-
-        if ($platform !== 'official') {
-            $positionIds = $clonePositions
-                ->andWhere('p.salesCompanyId = :salesCompanyId')
-                ->setParameter('salesCompanyId', $salesCompanyId)
-                ->getQuery()
-                ->getResult();
-        } else {
-            $positionIds = $clonePositions->getQuery()->getResult();
-        }
-
-        $positionIds = array_map('current', $positionIds);
-
-        // get all permissions of the given positions
-        return $this->getEntityManager()
+        $permission = $this->getEntityManager()
             ->createQueryBuilder()
-            ->select('
-                m.opLevel as op_level, 
-                p.id, 
-                max(p.name) as name, 
-                max(p.key) as key, 
-                max(m.positionId) as position_id'
-            )
-            ->from('SandboxApiBundle:Admin\AdminPositionPermissionMap', 'm')
-            ->join('m.permission', 'p')
-            ->where('m.positionId IN (:positionIds)')
-            ->setParameter('positionIds', $positionIds)
-            ->distinct()
-            ->groupBy('p.id', 'm.opLevel')
-            ->getQuery()
-            ->getResult();
+            ->select('ap.id, ap.name, ap.key, ap.maxOpLevel as op_level')
+            ->from('SandboxApiBundle:Admin\AdminPermission', 'ap')
+            ->where('ap.platform = :platform')
+            ->setParameter('platform', $platform);
+
+        if (!empty($excludePermissionIds)) {
+            $permission->andWhere('ap.id NOT IN (:excludePermissionIds)')
+                ->setParameter('excludePermissionIds', $excludePermissionIds);
+        }
+
+        return $permission->getQuery()->getResult();
     }
 
     private function findAdminExcludePermissionIds($platform, $salesCompanyId)
