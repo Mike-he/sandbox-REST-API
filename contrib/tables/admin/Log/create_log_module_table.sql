820d68d08bc9da72f2b5b7e6562cbc332c58e9bc
Mike He <mike.he@easylinks.com.cn>
2016-10-27 17:25:04 +0800

    fix log contrib

--- contrib/tables/admin/Log/create_log_module_table.sql
+++ contrib/tables/admin/Log/create_log_module_table.sql
@@ -5,17 +5,17 @@ CREATE TABLE `LogModules` (
   PRIMARY KEY (`id`)
  );
 
- INSERT INTO LogModules(`name`,`description`) VALUES('admin','管理员');
- INSERT INTO LogModules(`name`,`description`) VALUES('building','大楼');
- INSERT INTO LogModules(`name`,`description`) VALUES('invoice','发票');
- INSERT INTO LogModules(`name`,`description`) VALUES('room','房间');
- INSERT INTO LogModules(`name`,`description`) VALUES('room_order','房间订单');
- INSERT INTO LogModules(`name`,`description`) VALUES('room_order_reserve','预留');
- INSERT INTO LogModules(`name`,`description`) VALUES('room_order_preorder','预定');
- INSERT INTO LogModules(`name`,`description`) VALUES('user','用户');
- INSERT INTO LogModules(`name`,`description`) VALUES('product','商品');
+ INSERT INTO log_modules(`name`,`description`) VALUES('admin','管理员');
+ INSERT INTO log_modules(`name`,`description`) VALUES('building','大楼');
+ INSERT INTO log_modules(`name`,`description`) VALUES('invoice','发票');
+ INSERT INTO log_modules(`name`,`description`) VALUES('room','房间');
+ INSERT INTO log_modules(`name`,`description`) VALUES('room_order','房间订单');
+ INSERT INTO log_modules(`name`,`description`) VALUES('room_order_reserve','预留');
+ INSERT INTO log_modules(`name`,`description`) VALUES('room_order_preorder','预定');
+ INSERT INTO log_modules(`name`,`description`) VALUES('user','用户');
+ INSERT INTO log_modules(`name`,`description`) VALUES('product','商品');
 
 
---  INSERT INTO LogModules(`name`,`description`) VALUES('event','活动');
---  INSERT INTO LogModules(`name`,`description`) VALUES('price_rule','价格模版');
+--  INSERT INTO log_modules(`name`,`description`) VALUES('event','活动');
+--  INSERT INTO log_modules(`name`,`description`) VALUES('price_rule','价格模版');
 
