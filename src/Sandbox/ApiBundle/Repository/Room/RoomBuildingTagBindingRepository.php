58a0793ad8bc4d1d003705f92c1cd0a1f159edd3
Mike He <mike.he@easylinks.com.cn>
2016-11-12 18:03:40 +0800

    fix get building tag bug

--- src/Sandbox/ApiBundle/Repository/Room/RoomBuildingTagBindingRepository.php
+++ src/Sandbox/ApiBundle/Repository/Room/RoomBuildingTagBindingRepository.php
@@ -9,7 +9,12 @@ class RoomBuildingTagBindingRepository extends EntityRepository
     public function findRoomBuildingTagsByBuildingId($buildingId)
     {
         return $this->createQueryBuilder('rbt')
-            ->select('t.id, t.key, t.icon')
+            ->select('
+                t.id,
+                t.key,
+                t.icon,
+                t.iconWithBg
+            ')
             ->leftJoin('rbt.tag', 't')
             ->where('rbt.building = :buildingId')
             ->setParameter('buildingId', $buildingId)
