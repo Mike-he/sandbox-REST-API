45ef29e995f08883d274f10203d7c4d18c5edfe3
Mike He <mike.he@easylinks.com.cn>
2016-11-15 15:05:27 +0800

    fix building service bug

--- src/Sandbox/SalesApiBundle/Controller/Building/AdminBuildingController.php
+++ src/Sandbox/SalesApiBundle/Controller/Building/AdminBuildingController.php
@@ -1261,7 +1261,6 @@ class AdminBuildingController extends LocationController
             return;
         }
 
-        $buildingServices = array_unique($buildingServices);
         foreach ($buildingServices as $service) {
             if (!isset($service['id'])) {
                 continue;
