2223752af4cffe30be17cebf017c420b39d8d8ed
Mike He <mike.he@sandbox3.cn>
2016-11-23 14:07:33 +0800

    update migration bug

--- app/DoctrineMigrations/Version20161117023642.php
+++ app/DoctrineMigrations/Version20161117023642.php
@@ -17,7 +17,7 @@ class Version20161117023642 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql("UPDATE `admin_permission` SET `parentId`='48' WHERE `key` IN ('platform.order.reserve','platform.order.preorder','platform.room','platform.building','platform.product')");
-        $this->addSql("UPDATE `admin_permission` SET `parentId`='49' WHERE `key` IN ('sales.building.order.reserve','sales.building.order.preorder','sales.building.room','sales.building.building','sales.building.product')");
+        $this->addSql("UPDATE `admin_permission` SET `parentId`='49' WHERE `key` IN ('sales.building.order.reserve','sales.building.order.preorder','sales.building.room','sales.building.building','sales.building.product','sales.platform.building')");
     }
 
     /**
