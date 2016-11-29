e3f7ec7f85a9e36756c6730e4308fbe7e1eb1fa9
Mike He <mike.he@sandbox3.cn>
2016-11-29 16:53:19 +0800

    fix deploy shell

--- deployScript/deployment.sh
+++ deployScript/deployment.sh
@@ -7,3 +7,4 @@ sudo php app/console doc:mig:exe 20161116082531  # add admin_permission data
 sudo php app/console doc:mig:exe 20161117104305  # update room_types's type data
 sudo php app/console doc:mig:exe 20161118105827  # update admin_permission's name data
 sudo php app/console doc:mig:exe 20161125081731  # add room_city english name
+sudo php app/console doc:mig:exe 20161129070108  # add position & building service icons
