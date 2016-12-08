1b16388ae7f3b8572ab4c5e188dbe1ad14b616ff
Albert <albert.f@sandbox3.cn>
2016-12-08 10:03:05 +0800

    close #add new api patch lease status

--- src/Sandbox/SalesApiBundle/Controller/Lease/LeaseController.php
+++ src/Sandbox/SalesApiBundle/Controller/Lease/LeaseController.php
@@ -2,13 +2,57 @@
 
 namespace Sandbox\SalesApiBundle\Controller\Lease;
 
+use FOS\RestBundle\View\View;
+use Rs\Json\Patch;
+use Sandbox\ApiBundle\Form\Lease\LeasePatchType;
 use Sandbox\SalesApiBundle\Controller\SalesRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sandbox\ApiBundle\Entity\Lease\Lease;
+use Symfony\Component\HttpFoundation\Request;
 
 class LeaseController extends SalesRestController
 {
+    /**
+     * Patch Lease Status.
+     *
+     * @param $request
+     * @param $id
+     *
+     * @Route("/leases/{id}/status")
+     * @Method({"PATCH"})
+     *
+     * @throws \Exception
+     * @return View
+     */
+    public function patchLeaseStatusAction(
+        Request $request,
+        $id
+    ) {
+        // check user permission
+//        $this->checkAdminLeasePermission(AdminPermission::OP_LEVEL_EDIT);
+
+        $lease = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Lease\Lease')->find($id);
+
+        $this->throwNotFoundIfNull($lease, self::NOT_FOUND_MESSAGE);
+
+        $leaseJson = $this->container
+            ->get('serializer')
+            ->serialize($lease, 'json');
+
+        $patch = new Patch($leaseJson, $request->getContent());
+        $leaseJson = $patch->apply();
+
+        $form = $this->createForm(new LeasePatchType(), $lease);
+        $form->submit(json_decode($leaseJson, true));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return new View();
+    }
+
     /**
      * Delete Draft of Lease.
      *
