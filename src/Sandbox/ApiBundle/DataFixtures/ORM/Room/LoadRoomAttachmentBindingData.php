601c0be54c727dc8efb6f7a07aab6dacd85e3f73
feng.li <feng.li@sandbox3.cn>
2017-01-07 11:04:28 +0800

    fix fixture load data bug

--- src/Sandbox/ApiBundle/DataFixtures/ORM/Room/LoadRoomAttachmentBindingData.php
+++ src/Sandbox/ApiBundle/DataFixtures/ORM/Room/LoadRoomAttachmentBindingData.php
@@ -19,8 +19,13 @@ class LoadRoomAttachmentBindingData extends AbstractFixture implements OrderedFi
         $rab2->setRoom($this->getReference('fixed-room-for-get-spaces-data-structure'));
         $rab2->setAttachmentId($this->getReference('room-attachment-2'));
 
+        $rab3 = new RoomAttachmentBinding();
+        $rab3->setRoom($this->getReference('longterm-room-1'));
+        $rab3->setAttachmentId($this->getReference('room-attachment-longterm'));
+
         $manager->persist($rab1);
         $manager->persist($rab2);
+        $manager->persist($rab3);
 
         $manager->flush();
     }
