63fdea3c5aae30ac29add73af36e5022186a015c
Leo Xu <leox8341@qq.com>
2017-01-16 17:15:13 +0800

    update migration for log module

---
+++ app/DoctrineMigrations/Version20170111090021.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Sandbox\ApiBundle\Entity\Log\LogModules;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170111090021 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+    }
+
+    public function postUp(Schema $schema)
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $module1 = new LogModules();
+        $module1->setName('product_appointment');
+        $module1->setDescription('长租申请');
+
+        $module2 = new LogModules();
+        $module2->setName('lease');
+        $module2->setDescription('长租合同');
+
+        $em->persist($module1);
+        $em->persist($module2);
+
+        $em->flush();
+    }
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
