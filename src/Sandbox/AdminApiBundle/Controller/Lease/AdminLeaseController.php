c74c53094ae0f32663c42665a375b8b41ee39cdc
Albert <albert.f@sandbox3.cn>
2017-02-24 14:22:58 +0800

    Revert "Remove drawee of lease"
    This reverts commit ad030a413086b2e975ccab0f31f5f5c94fccdc2c.

--- src/Sandbox/AdminApiBundle/Controller/Lease/AdminLeaseController.php
+++ src/Sandbox/AdminApiBundle/Controller/Lease/AdminLeaseController.php
@@ -101,6 +101,10 @@ class AdminLeaseController extends AdminRestController
                 'type' => LeaseBill::TYPE_LEASE,
             ));
 
+        $drawee = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:User\UserView')
+            ->find($lease->getDrawee()->getId());
+
         $supervisor = $this->getDoctrine()
             ->getRepository('SandboxApiBundle:User\UserView')
             ->find($lease->getSupervisor()->getId());
@@ -111,7 +115,9 @@ class AdminLeaseController extends AdminRestController
 
         $html = $this->renderView(':Leases:leases_print.html.twig', array(
             'lease' => $lease,
+            'drawee' => $drawee,
             'supervisor' => $supervisor,
+            'draweeAvatarUrl' => $this->generateAvatarUrl($drawee->getId()),
             'supervisorAvatarUrl' => $this->generateAvatarUrl($supervisor->getId()),
             'excludeTypes' => $excludeLeaseRentTypes,
             'bills' => $bills,
