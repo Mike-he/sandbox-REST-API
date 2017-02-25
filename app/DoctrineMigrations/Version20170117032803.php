3ef9a5d5b4362ce838b4c718c5900b0d522234f2
Mike He <mike.he@sandbox3.cn>
2017-02-25 14:45:37 +0800

    fix migration bug

--- app/DoctrineMigrations/Version20170117032803.php
+++ app/DoctrineMigrations/Version20170117032803.php
@@ -4,17 +4,12 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
-use Sandbox\ApiBundle\Entity\SalesAdmin\SalesCompanyServiceInfos;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170117032803 extends AbstractMigration implements ContainerAwareInterface
+class Version20170117032803 extends AbstractMigration
 {
-    use ContainerAwareTrait;
-
     /**
      * @param Schema $schema
      */
@@ -24,38 +19,6 @@ class Version20170117032803 extends AbstractMigration implements ContainerAwareI
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE lease_bill ADD sales_invoice TINYINT(1) NOT NULL, ADD invoiced TINYINT(1) NOT NULL');
-    }
-
-    /**
-     * @param Schema $schema
-     */
-    public function postUp(Schema $schema)
-    {
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        $bills = $em->getRepository('SandboxApiBundle:Lease\LeaseBill')
-            ->findAll();
-
-        foreach ($bills as $bill) {
-            $salesCompany = $bill->getLease()->getProduct()->getRoom()->getBuilding()->getCompany();
-            $serviceInfo = $em->getRepository('SandboxApiBundle:SalesAdmin\SalesCompanyServiceInfos')
-                ->findOneBy(array(
-                    'company' => $salesCompany,
-                    'tradeTypes' => 'longterm',
-                    'status' => true,
-                ));
-
-            if (is_null($serviceInfo)) {
-                continue;
-            }
-
-            if ($serviceInfo->getDrawer() == SalesCompanyServiceInfos::DRAWER_SALES) {
-                $bill->setSalesInvoice(true);
-            }
-        }
-
-        $em->flush();
-        $em->clear();
     }
 
     /**
