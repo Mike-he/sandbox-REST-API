18b323a581aafea96a664955e993354c85a79d40
Mike He <mike.he@sandbox3.cn>
2017-03-14 18:06:30 +0800

    add client version check api

---
+++ app/DoctrineMigrations/data/Version920170314095949.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Sandbox\ApiBundle\Controller\Version\ClientVersionCheckController;
+use Sandbox\ApiBundle\Entity\Parameter\Parameter;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version920170314095949 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function postUp(Schema $schema)
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $parameter = new Parameter();
+        $parameter->setKey(ClientVersionCheckController::UPDATE_REQUIRED_LAST_VERSION);
+        $parameter->setValue('');
+        $em->persist($parameter);
+
+        $em->flush();
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
