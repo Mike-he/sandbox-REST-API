5f8e4855bd097478cb476bac16baaed2afdb5991
Mike He <mike.he@sandbox3.cn>
2017-04-19 15:08:52 +0800

    fix migration bug

---
+++ app/DoctrineMigrations/data/Version920170419065719.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version920170419065719 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function postUp(Schema $schema)
+    {
+        parent::postUp($schema);
+
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $productOrders = $em->getRepository('SandboxApiBundle:Order\ProductOrder')
+            ->findAll();
+
+        foreach ($productOrders as $productOrder) {
+            $productOrder->setPaymentUserId($productOrder->getUserId());
+        }
+
+        $leassBills = $em->getRepository('SandboxApiBundle:Lease\LeaseBill')
+            ->findAll();
+
+        foreach ($leassBills as $bill) {
+            $bill->setPaymentUserId($bill->getDrawee());
+        }
+
+        $em->flush();
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
