d69c252999859c5498fffe7e0f1fac529038624b
Mike He <mike.he@sandbox3.cn>
2017-06-23 17:15:16 +0800

    fix bug

--- src/Sandbox/ApiBundle/Traits/OpenfireApi.php
+++ src/Sandbox/ApiBundle/Traits/OpenfireApi.php
@@ -143,4 +143,71 @@ trait OpenfireApi
 
         return $message;
     }
+
+    /**
+     * @param $fromJID
+     * @param $toJID
+     * @param $type
+     *
+     * @return array
+     */
+    protected function getHistoryMessageForService(
+        $fromJID,
+        $toJID,
+        $type,
+        $offset = null,
+        $limit = null
+    ) {
+        header('Content-type:text/html;charset=utf-8');  //统一输出编码为utf-8
+
+        $db_host = $this->getContainer()->getParameter('database_host');
+        $db_name = $this->getContainer()->getParameter('openfire_db_name');
+        $db_user = $this->getContainer()->getParameter('openfire_db_user');
+        $db_pwd = $this->getContainer()->getParameter('openfire_db_password');
+
+        $mysqli = mysqli_connect($db_host, $db_user, $db_pwd, $db_name);
+
+        if (!$mysqli) {
+            echo mysqli_connect_error();
+        }
+
+        mysqli_query($mysqli, 'set names utf8');
+
+        $query = "select * from ofmessagearchive where status = 'message' and type = $type";
+
+        $query .=  "and ((toJID = $toJID and fromJID = $fromJID) or (toJID = $fromJID and fromJID = $toJID ))";
+
+        $query .= " order by sentDate DESC";
+
+        if ($offset && $limit) {
+            $query .= " limit ".$offset.','.$limit;
+        }
+
+        $result = $mysqli->query($query);
+        $message = array();
+        if ($result) {
+            if ($result->num_rows > 0) {                        //判断结果集中行的数目是否大于0
+                while ($row = $result->fetch_array()) {       //循环输出结果集中的记录
+                    $message[] = array(
+                        'messageID' => $row['messageID'],
+                        'conversationID' => $row['conversationID'],
+                        'fromJID' => $row['fromJID'],
+                        'fromJIDResource' => $row['fromJIDResource'],
+                        'toJID' => $row['toJID'],
+                        'toJIDResource' => $row['toJIDResource'],
+                        'sentDate' => $row['sentDate'],
+                        'body' => $row['body'],
+                        'type' => $row['type'],
+                        'status' => $row['status'],
+                        'company' => $row['company'],
+                    );
+                }
+            }
+        } else {
+            echo '查询失败';
+        }
+        $mysqli->close();
+
+        return $message;
+    }
 }
