c24eb5a2751bcd2fabf8d9a5f8dbbd7b4c30d961
feng.li <feng.li@sandbox3.cn>
2017-06-27 10:58:19 +0800

    fix order bug

--- src/Sandbox/ApiBundle/Repository/Order/OrderRepository.php
+++ src/Sandbox/ApiBundle/Repository/Order/OrderRepository.php
@@ -856,12 +856,12 @@ class OrderRepository extends EntityRepository
         $query = $this->createQueryBuilder('o')
             ->where('o.productId = :productId')
             ->andWhere('o.status <> \'cancelled\'')
-            ->andWhere('o.startDate >= :startDate')
-            ->andWhere('o.endDate <= :endDate')
+            ->andWhere('o.endDate >= :startDate')
+//            ->andWhere('o.endDate <= :endDate')
             ->orderBy('o.startDate', 'ASC')
             ->setParameter('productId', $id)
             ->setParameter('startDate', $startDate)
-            ->setParameter('endDate', $endDate)
+//            ->setParameter('endDate', $endDate)
             ->getQuery();
 
         return $query->getResult();
