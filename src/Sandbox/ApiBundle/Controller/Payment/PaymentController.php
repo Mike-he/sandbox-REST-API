f4d294d220e4b4e25cfee9da941c4a87d33211af
Mike He <mike.he@sandbox3.cn>
2017-07-31 17:23:07 +0800

    fix bug

--- src/Sandbox/ApiBundle/Controller/Payment/PaymentController.php
+++ src/Sandbox/ApiBundle/Controller/Payment/PaymentController.php
@@ -1312,7 +1312,7 @@ class PaymentController extends DoorController
         $startDate = $this->getLastMembershipOrderEndDate($userId, $card);
         $endDate = clone $startDate;
         $endDate->modify("+$validPeriod $unit");
-        $endDate->modify('-1 second');
+//        $endDate->modify('-1 second');
 
         $order = new MembershipOrder();
         $order->setUser($userId);
