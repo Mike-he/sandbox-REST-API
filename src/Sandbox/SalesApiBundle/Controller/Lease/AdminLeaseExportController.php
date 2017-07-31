3e8b5bed52ad17fd3f87919fb01263807a5de018
feng.li <feng.li@sandbox3.cn>
2017-07-31 13:48:42 +0800

    fix bug

--- src/Sandbox/SalesApiBundle/Controller/Lease/AdminLeaseExportController.php
+++ src/Sandbox/SalesApiBundle/Controller/Lease/AdminLeaseExportController.php
@@ -842,17 +842,12 @@ class AdminLeaseExportController extends SalesRestController
         // create the response
         $response = $this->get('phpexcel')->createStreamedResponse($writer);
 
-        $fileName = ord($fileName);
-        // adding headers
-        $dispositionHeader = $response->headers->makeDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            $fileName.'.xls'
-        );
+        $filename = urlencode($fileName.'.xls');
 
         $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
         $response->headers->set('Pragma', 'public');
         $response->headers->set('Cache-Control', 'maxage=1');
-        $response->headers->set('Content-Disposition', $dispositionHeader);
+        $response->headers->set('Content-Disposition', 'attachment;filename='.$filename);
 
         return $response;
     }
