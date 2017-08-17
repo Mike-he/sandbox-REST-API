31e646742b8624b7cb64c5245f55e910c5677b64
athy <athy_hou@163.com>
2017-08-17 14:37:00 +0800

    fix

--- src/Sandbox/ApiBundle/Repository/Customer/UserEnterpriseCustomerRepository.php
+++ src/Sandbox/ApiBundle/Repository/Customer/UserEnterpriseCustomerRepository.php
@@ -53,4 +53,46 @@ class UserEnterpriseCustomerRepository extends EntityRepository
 
         return $query->getQuery()->getResult();
     }
+
+    /**
+     * @param $salesCompanyId
+     * @param $keyword
+     * @param $keywordSearch
+     * @return mixed
+     */
+    public function getCountSalesEnterpriseCustomers(
+        $salesCompanyId,
+        $keyword,
+        $keywordSearch
+    ) {
+        $query = $this->createQueryBuilder('ec')
+            ->select('COUNT(ec)')
+            ->leftJoin('SandboxApiBundle:User\EnterpriseCustomerContacts', 'ecc', 'WITH', 'ec.id = ecc.enterpriseCustomerId')
+            ->leftJoin('SandboxApiBundle:User\UserCustomer', 'uc', 'WITH', 'ecc.customerId = uc.id');
+
+        $query->where('ec.companyId = :companyId')
+            ->setParameter('companyId', $salesCompanyId);
+
+        if(!is_null($keyword) && !is_null($keywordSearch)){
+            switch ($keyword) {
+                case 'name':
+                    $query->andWhere('ec.name LIKE :search');
+                    break;
+                case 'registerAddress':
+                    $query->andWhere('ec.registerAddress LIKE :search');
+                    break;
+                case 'contactName':
+                    $query->andWhere('uc.name LIKE :search');
+                    break;
+                case 'contactPhone':
+                    $query->andWhere('uc.phone LIKE :search');
+                    break;
+                default:
+                    break;
+            }
+            $query->setParameter('search', '%'.$keywordSearch.'%');
+        }
+
+        return $query->getQuery()->getSingleScalarResult();
+    }
 }
