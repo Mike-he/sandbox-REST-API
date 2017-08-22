ed447f6f7d07f42292e31947b0785398e3a0fe6a
Mike He <mike.he@sandbox3.cn>
2017-08-22 14:53:55 +0800

    fix profile lessor api bug

---
+++ src/Sandbox/ApiBundle/Form/SalesAdmin/SalesFinanceProfileLessorPatchType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sandbox\ApiBundle\Form\SalesAdmin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class SalesFinanceProfileLessorPatchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('company_name')
+            ->add('address')
+            ->add('contact_name')
+            ->add('phone')
+            ->add('email');
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sandbox\ApiBundle\Entity\SalesAdmin\SalesCompanyProfileLessor',
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return '';
+    }
+}
