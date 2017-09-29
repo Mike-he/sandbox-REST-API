63379faf13af1ec7e17829e958fcde1cd3bf502a
feng.li <feng.li@sandbox3.cn>
2017-09-29 12:59:21 +0800

    add sync door access data

---
+++ src/Sandbox/AdminApiBundle/Command/SyncDoorAccessCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Sandbox\AdminApiBundle\Command;
+
+use Doctrine\ORM\EntityManager;
+use Sandbox\ApiBundle\Entity\Room\RoomBuilding;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Sandbox\ApiBundle\Traits\DoorAccessTrait;
+
+class SyncDoorAccessCommand extends ContainerAwareCommand
+{
+    use DoorAccessTrait;
+
+    protected function configure()
+    {
+        $this->setName('sandbox:api-bundle:sync:DoorAccess')
+            ->setDescription('Sync user card and room order in door access')
+            ->addArgument('userId', InputArgument::REQUIRED, 'user ID')
+            ->addArgument('roomId', InputArgument::REQUIRED, 'room ID')
+            ->addArgument('orderId', InputArgument::REQUIRED, 'order ID')
+            ->addArgument('type', InputArgument::REQUIRED, 'type: order or lease');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $arguments = $input->getArguments();
+        $userId = $arguments['userId'];
+        $roomId = $arguments['room'];
+        $orderId = $arguments['order'];
+        $type = $arguments['type'];
+
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $user = $em->getRepository('SandboxApiBundle:User\User')->find($userId);
+        $cardNo = $user->getCardNo();
+
+        if ($type == 'order') {
+            $order = $em->getRepository('SandboxApiBundle:Order\ProductOrder')->find($orderId);
+
+            $accessNo = $order->getId();
+            $start = $order->getStartDate();
+            $end = $order->getEndDate();
+        } elseif ($type == 'lease') {
+            $lease = $em->getRepository('SandboxApiBundle:Lease\Lease')->find($orderId);
+
+            $start = $lease->getStartDate();
+            $end = $lease->getEndDate();
+            $accessNo = $lease->getAccessNo();
+        }
+
+        $roomDoors = $em
+            ->getRepository('SandboxApiBundle:Room\RoomDoors')
+            ->findBy(['room' => $roomId]);
+
+        $room = $em->getRepository('SandboxApiBundle:Room\Room')->find($roomId);
+
+        /** @var RoomBuilding $building */
+        $building = $room->getBuilding();
+
+        $base = $building->getServer();
+
+        try {
+            $this->setEmployeeCardForOneBuilding(
+                $base,
+                $userId,
+                $cardNo
+            );
+            sleep(10);
+            $userArray = [
+                ['empid' => "$userId"],
+            ];
+
+            $this->setRoomOrderAccessIfUserArray(
+                $base,
+                $userArray,
+                $roomDoors,
+                $accessNo,
+                $start,
+                $end
+            );
+        } catch (\Exception $e) {
+            error_log('Set card and room door access went wrong!');
+        }
+    }
+}
