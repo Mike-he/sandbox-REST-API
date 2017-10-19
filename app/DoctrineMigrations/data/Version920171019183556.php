b3ac3771edcd044be3541dfbe48f5119632085fd
feng.li <feng.li@sandbox3.cn>
2017-10-19 18:00:53 +0800

    fix bug

---
+++ app/DoctrineMigrations/data/Version920171019183556.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\ORM\EntityManager;
+use Sandbox\ApiBundle\Entity\Event\Event;
+use Sandbox\ApiBundle\Entity\User\UserCustomer;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version920171019183556 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+
+    public function postUp(Schema $schema)
+    {
+        parent::postUp($schema);
+
+        /** @var EntityManager $em */
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $events = $em->getRepository('SandboxApiBundle:Event\Event')->findAll();
+
+        foreach ($events as $event) {
+            if ($event->isSaved()){
+                $event->setStatus(Event::STATUS_SAVED);
+            } else {
+                $event->setStatus(Event::STATUS_PREHEATING);
+            }
+        }
+
+        $em->flush();
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
