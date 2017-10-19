1150ead5198e8acf07f0d0d7777f46da6aeee4d4
Mike He <mike.he@sandbox3.cn>
2017-10-19 13:03:16 +0800

    add migration file

---
+++ app/DoctrineMigrations/Version20171018094041.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171018094041 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sales_admin_profiles DROP admin_id, CHANGE avatar avatar VARCHAR(255) DEFAULT NULL, CHANGE gender gender VARCHAR(32) DEFAULT NULL, CHANGE nickname nickname VARCHAR(255) DEFAULT NULL, CHANGE email email VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sales_admin_profiles ADD admin_id INT NOT NULL, CHANGE avatar avatar VARCHAR(255) NOT NULL, CHANGE gender gender VARCHAR(32) NOT NULL, CHANGE nickname nickname VARCHAR(255) NOT NULL, CHANGE email email VARCHAR(255) NOT NULL');
+    }
+}
