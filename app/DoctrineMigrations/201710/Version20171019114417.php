52452fff0d1b53c97eeaf88a71c60676681ed8c5
feng.li <feng.li@sandbox3.cn>
2017-10-19 17:45:08 +0800

    fix bug

---
+++ app/DoctrineMigrations/201710/Version20171019114417.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171019114417 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product_order ADD CONSTRAINT FK_5475E8C464B64DCC FOREIGN KEY (userId) REFERENCES user (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE event ADD status VARCHAR(64) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event DROP status');
+        $this->addSql('ALTER TABLE product_order DROP FOREIGN KEY FK_5475E8C464B64DCC');
+    }
+}
