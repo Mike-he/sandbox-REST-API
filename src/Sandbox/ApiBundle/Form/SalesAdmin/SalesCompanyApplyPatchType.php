007107b7b1a2af88606c6cd7058a3b29c812d8b3
Mike He <mike.he@sandbox3.cn>
2017-10-21 13:37:31 +0800

    add patch company applications api

---
+++ src/Sandbox/ApiBundle/Form/SalesAdmin/SalesCompanyApplyPatchType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sandbox\ApiBundle\Form\SalesAdmin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SalesCompanyApplyPatchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('status')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sandbox\ApiBundle\Entity\SalesAdmin\SalesCompanyApply',
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return '';
+    }
+}
