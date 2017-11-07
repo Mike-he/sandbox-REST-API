6897cb807a139a80f44e977f36b217cf0bc21a2c
Mike He <mike.he@sandbox3.cn>
2017-11-07 15:35:27 +0800

    fix bug

--- homestead/entrypoint.sh
+++ homestead/entrypoint.sh
@@ -11,8 +11,8 @@ cp data/pdf_bin/* /usr/bin/ && chmod +x /usr/bin/wkhtmltopdf
 # Copy composer on system and install it globally
 cp data/composer.phar /usr/local/bin/composer && chmod +x /usr/local/bin/composer
 
-HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
-setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX app/cache app/logs
+HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1) && \
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX app/cache app/logs && \
 setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX app/cache app/logs
 
 # Startup
