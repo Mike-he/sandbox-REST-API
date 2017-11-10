829b5a6389966dd6827138412bdb2eedaa54130e
feng.li <feng.li@sandbox3.cn>
2017-11-10 16:52:12 +0800

    fix bug

--- src/Sandbox/ClientPropertyApiBundle/Controller/Order/ClientEventOrderController.php
+++ src/Sandbox/ClientPropertyApiBundle/Controller/Order/ClientEventOrderController.php
@@ -181,12 +181,16 @@ class ClientEventOrderController extends SalesRestController
         $payEnd = $paramFetcher->get('pay_end');
 
         $createStart = $paramFetcher->get('create_start');
-        $createStart = new \DateTime($createStart);
+        if ($createStart) {
+            $createStart = new \DateTime($createStart);
+        }
 
         $createEnd = $paramFetcher->get('create_end');
-        $createEnd = new \DateTime($createEnd);
-        $createEnd->setTime(23, 59, 59);
-
+        if ($createEnd) {
+            $createEnd = new \DateTime($createEnd);
+            $createEnd->setTime(23, 59, 59);
+        }
+        
         $eventStart = $paramFetcher->get('event_start');
         $eventEnd = $paramFetcher->get('event_end');
 
