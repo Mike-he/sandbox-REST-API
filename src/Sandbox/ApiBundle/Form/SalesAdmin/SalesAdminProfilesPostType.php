f30a83f544f8df0f76e6220b0f2c47931734df84
feng.li <feng.li@sandbox3.cn>
2017-11-16 10:21:08 +0800

    fix bug

--- src/Sandbox/ApiBundle/Form/SalesAdmin/SalesAdminProfilesPostType.php
+++ src/Sandbox/ApiBundle/Form/SalesAdmin/SalesAdminProfilesPostType.php
@@ -3,8 +3,10 @@
 namespace Sandbox\ApiBundle\Form\SalesAdmin;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SalesAdminProfilesPostType extends AbstractType
 {
@@ -15,7 +17,14 @@ class SalesAdminProfilesPostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('sales_company_id')
+            ->add('sales_company_id',
+                TextType::class,
+                array(
+                    'constraints' => array(
+                        new NotBlank(),
+                    ),
+                )
+            )
             ->add('avatar')
             ->add('gender')
             ->add('nickname')
