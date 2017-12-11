c631114239b4d7074a476feba80f3ce55185333e
Mike He <mike.he@sandbox3.cn>
2017-12-11 11:13:18 +0800

    add dashboard api

--- src/Sandbox/CommnueAdminApiBundle/Controller/Dashboard/AdminDashboardController.php
+++ src/Sandbox/CommnueAdminApiBundle/Controller/Dashboard/AdminDashboardController.php
@@ -6,6 +6,9 @@ use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations;
 use Sandbox\ApiBundle\Controller\SandboxRestController;
+use Sandbox\ApiBundle\Entity\Admin\AdminPosition;
+use Sandbox\ApiBundle\Entity\Event\Event;
+use Sandbox\ApiBundle\Entity\Room\RoomBuilding;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -68,6 +71,130 @@ class AdminDashboardController extends SandboxRestController
             'total_users' => (int) $result['total'],
             'register_users' => $month,
             'auth_users' => $response['auth_users'],
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param ParamFetcherInterface $paramFetcher
+     *
+     * @Route("/dashboard/statistics")
+     * @Method({"GET"})
+     *
+     * @return View
+     */
+    public function getDashboardStatisticsAction(
+        Request $request,
+        ParamFetcherInterface $paramFetcher
+    ) {
+        $positions = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Admin\AdminPosition')
+            ->findBy(['platform' => AdminPosition::PLATFORM_COMMNUE]);
+
+        $adminsSum = [];
+        foreach ($positions as $position) {
+            $admins = $this->getDoctrine()
+                ->getRepository('SandboxApiBundle:Admin\AdminPositionUserBinding')
+                ->findBy(['positionId' => $position->getId()]);
+
+            foreach ($admins as $admin) {
+                array_push($adminsSum, $admin->getUserId());
+            }
+        }
+
+        $adminsSum = array_unique($adminsSum);
+
+        $communities = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Room\RoomBuilding')
+            ->findBy(['isDeleted' => false]);
+
+        $spaces = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Product\Product')
+            ->findBy([
+                'isDeleted' => false,
+                'visible' => false,
+            ]);
+
+        $authedCommunities = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Room\RoomBuilding')
+            ->findBy([
+                'isDeleted' => false,
+                'commnueStatus' => RoomBuilding::CERTIFIED,
+            ]);
+
+        $bannedCommunities = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Room\RoomBuilding')
+            ->findBy([
+                'isDeleted' => false,
+                'commnueStatus' => RoomBuilding::FREEZON,
+            ]);
+
+        $activities = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Event\Event')
+            ->findBy([
+                'platform' => Event::PLATFORM_COMMNUE,
+                'isDeleted' => false,
+                'visible' => true,
+                'status' => [
+                    Event::STATUS_WAITING,
+                    Event::STATUS_END,
+                    Event::STATUS_ONGOING,
+                    Event::STATUS_PREHEATING,
+                    Event::STATUS_REGISTERING,
+                ]
+            ]);
+
+        $registrationsSum = [];
+        foreach ($activities as $activity) {
+            $registrations = $this->getDoctrine()
+                ->getRepository('SandboxApiBundle:Event\EventRegistration')
+                ->findBy(['eventId' => $activity->getId()]);
+
+            array_push($registrationsSum, $registrations);
+        }
+
+        $ongoingActivities = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Event\Event')
+            ->findBy([
+                'platform' => Event::PLATFORM_COMMNUE,
+                'isDeleted' => false,
+                'visible' => true,
+                'status' => [
+                    Event::STATUS_WAITING,
+                    Event::STATUS_ONGOING,
+                    Event::STATUS_PREHEATING,
+                    Event::STATUS_REGISTERING,
+                ]
+            ]);
+
+        $endedActivities = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Event\Event')
+            ->findBy([
+                'platform' => Event::PLATFORM_COMMNUE,
+                'isDeleted' => false,
+                'visible' => true,
+                'status' => [
+                    Event::STATUS_END,
+                ]
+            ]);
+
+        return new View([
+            'administrator' => [
+                'current_positions' => count($positions),
+                'current_administrators' => count($adminsSum),
+            ],
+            'community' => [
+                'current_communities' => count($communities),
+                'current_space' => count($spaces),
+                'authed_companies' => count($authedCommunities),
+                'banned_companies' => count($bannedCommunities),
+            ],
+            'activity' => [
+                'current_activities' => count($activities),
+                'registrations' => count($registrationsSum),
+                'ongoing_activities' => count($ongoingActivities),
+                'ended_activities' => count($endedActivities),
+            ],
         ]);
     }
 }
