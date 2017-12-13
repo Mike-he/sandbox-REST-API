5bf1919d9264c001f1bc3f8ce862db981ed75eda
athy <athy_hou@163.com>
2017-12-13 14:16:26 +0800

    fix commune app community

--- src/Sandbox/ApiBundle/Repository/Room/RoomBuildingRepository.php
+++ src/Sandbox/ApiBundle/Repository/Room/RoomBuildingRepository.php
@@ -847,6 +847,7 @@ class RoomBuildingRepository extends EntityRepository
     ) {
         $query = $this->createQueryBuilder('rb')
             ->leftJoin('SandboxApiBundle:Room\Room','r','WITH','r.buildingId = rb.id')
+            ->leftJoin('SandboxApiBundle:Room\RoomBuildingAttachment','rba','WITH','rba.buildingId = rb.id')
                       ->select('
                         rb.id,
                         rb.name,
@@ -861,6 +862,7 @@ class RoomBuildingRepository extends EntityRepository
                         rb.address,
                         rb.lat,
                         rb.lng,
+                        rba.content as attachment,
                         (rb.orderEvaluationNumber + rb.buildingEvaluationNumber) as total_comments_amount
                     ')
                         ->where('rb.commnueStatus != :commnuestatus')
