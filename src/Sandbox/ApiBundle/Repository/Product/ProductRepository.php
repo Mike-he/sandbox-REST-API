6fb4ff00416c08ab91fd837fa977e16640c4f5e2
athy <athy_hou@163.com>
2017-12-15 17:56:11 +0800

    fix

--- src/Sandbox/ApiBundle/Repository/Product/ProductRepository.php
+++ src/Sandbox/ApiBundle/Repository/Product/ProductRepository.php
@@ -2004,6 +2004,25 @@ class ProductRepository extends EntityRepository
 //            $stat->execute();
 //            $pidsTwo = array_map('current', $stat->fetchAll());
 
+            $em = $this->getEntityManager();
+            $connection = $em->getConnection();
+            $stat = $connection->prepare("
+                    SELECT p.id,r.allowedPeople
+                    FROM product AS p
+                      LEFT JOIN product_order AS po ON po.productId = p.id
+                      LEFT JOIN room AS r ON r.id = p.roomId
+                    WHERE r.type = '$roomType'
+                    AND ((po.startDate >= $startDate AND po.endDate <= $endDate) OR
+                       (po.startDate <= $startDate AND po.endDate >= $startDate) OR
+                       (po.startDate <= $endDate AND po.endDate >= $endDate))
+                    AND po.s
+                    GROUP BY p.id
+                    HAVING COUNT(po.id) < r.allowedPeople
+                    ;
+              ");
+            $stat->execute();
+            $pidsTwo = array_map('current', $stat->fetchAll());
+
             $query->andWhere('p.startDate <= :startDate')
                 ->andWhere('p.endDate >= :startDate')
                 ->andWhere('p.id IN (:pids)')
