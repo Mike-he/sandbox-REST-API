89bb12304862874f4e7919f720d44e64c88352eb
feng.li <feng.li@sandbox3.cn>
2018-01-02 10:44:00 +0800

    add

--- src/Sandbox/CommnueClientApiBundle/Controller/Expert/ClientExpertController.php
+++ src/Sandbox/CommnueClientApiBundle/Controller/Expert/ClientExpertController.php
@@ -2,14 +2,14 @@
 
 namespace Sandbox\CommnueClientApiBundle\Controller\Expert;
 
-use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\View\View;
+use Sandbox\ApiBundle\Entity\Expert\Expert;
+use Sandbox\ApiBundle\Form\Expert\ExpertPostType;
 use Sandbox\SalesApiBundle\Controller\SalesRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use FOS\RestBundle\Controller\Annotations;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class ClientExpertController extends SalesRestController
 {
@@ -24,9 +24,23 @@ class ClientExpertController extends SalesRestController
      * @return View
      */
     public function checkExpertAction(
-
+        Request $request
     ) {
+        $user = $this->getUser();
 
+        $expert = $this->getDoctrine()
+            ->getRepository('SandboxApiBundle:Expert\Expert')
+            ->findOneBy(array('userId' => $user->getUserId()));
+
+        $response = array();
+        if ($expert) {
+            $response['status'] = true;
+            $response['banned'] = $expert->isBanned();
+        } else {
+            $response['status'] = false;
+        }
+
+        return new View($response);
     }
 
     /**
@@ -42,10 +56,35 @@ class ClientExpertController extends SalesRestController
     public function postExpertAction(
         Request $request
     ) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
 
-        $view = new View();
-        $view->setStatusCode(201);
+        $expert = new Expert();
 
-        return $view;
+        $form = $this->createForm(new ExpertPostType(), $expert);
+        $form->handleRequest($request);
+
+        if (!$form->isValid()) {
+            throw new BadRequestHttpException(self::BAD_PARAM_MESSAGE);
+        }
+
+        $requestContent = json_decode($request->getContent(), true);
+
+        $fieldIds = $requestContent['field_ids'];
+
+        foreach ($fieldIds as $fieldId) {
+
+        }
+
+        $expert->setUserId($user->getUserId());
+        $em->persist($expert);
+
+        $em->flush();
+
+        $response = array(
+            'id' => $expert->getId(),
+        );
+
+        return new View($response, 201);
     }
 }
