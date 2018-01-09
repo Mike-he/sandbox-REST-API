3793c3145a996fb527edafa0391337c2ac04d1be
Mike He <mike.he@sandbox3.cn>
2018-01-09 17:05:37 +0800

    fix bug

--- src/Sandbox/ApiBundle/Repository/Expert/ExpertOrderRepository.php
+++ src/Sandbox/ApiBundle/Repository/Expert/ExpertOrderRepository.php
@@ -6,24 +6,44 @@ use Doctrine\ORM\EntityRepository;
 
 class ExpertOrderRepository extends EntityRepository
 {
+    /**
+     * @param $expert
+     * @param $status
+     * @param $limit
+     * @param $offset
+     * @param null $userId
+     *
+     * @return array
+     */
     public function getLists(
-       $expert,
-       $status,
-       $limit,
-       $offset
+        $expert,
+        $status,
+        $limit,
+        $offset,
+        $userId = null
     ) {
         $query = $this->createQueryBuilder('o')
             ->select('
                  o.id,
                  o.userId as user_id,
+                 o.expertId as expert_id,
                  o.orderNumber as order_number,
                  o.status,
                  o.creationDate as creation_date,
                  o.cancelledDate as cancelled_date,
                  o.completedDate as completed_date
             ')
-            ->where('o.expertId = :expert')
-            ->setParameter('expert', $expert);
+            ->where('o.id > 0');
+
+        if (!is_null($userId)) {
+            $query->andWhere('o.userId = :userId')
+                ->setParameter('userId', $userId);
+        }
+
+        if (!is_null($expert)) {
+            $query->andWhere('o.expertId = :expert')
+                ->setParameter('expert', $expert);
+        }
 
         if ($status) {
             $query->andWhere('o.status = :status')
