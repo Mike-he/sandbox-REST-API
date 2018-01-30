0d0d7e839e70022565c4ce23179c430a7dd47d47
Mike He <mike.he@sandbox3.cn>
2018-01-30 16:44:07 +0800

    add event generic list

--- src/Sandbox/ApiBundle/Entity/GenericList/GenericList.php
+++ src/Sandbox/ApiBundle/Entity/GenericList/GenericList.php
@@ -27,6 +27,7 @@ class GenericList
     const OBJECT_MEMBERSHIP_ORDER = 'membership_order';
     const OBJECT_SERVICE_ORDER = 'service_order';
     const OBJECT_COMMNUE_COMPANY = 'commnue_company';
+    const OBJECT_ACTIVITY = 'activity';
 
     const OBJECT_CUSTOMER = 'customer';
     const OBJECT_ENTERPRISE = 'enterprise';
