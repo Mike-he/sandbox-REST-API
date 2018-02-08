d4d49984105070048d707530a0c441d6fc3f5dc6
athy <athy_hou@163.com>
2018-02-08 14:15:47 +0800

    fix

---
+++ app/DoctrineMigrations/data/Version920180208132701.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\ORM\EntityManager;
+use Sandbox\ApiBundle\Entity\Event\Event;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version920180208132701 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function postUp(Schema $schema)
+    {
+        parent::postUp($schema);
+
+        /** @var EntityManager $em */
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $events = $em->getRepository('SandboxApiBundle:Event\Event')
+            ->findBy([
+                'platform' => Event::PLATFORM_COMMNUE,
+                'visible' => true,
+                'isDeleted' => false
+            ]);
+
+        foreach ($events as $event) {
+            $event->setCommnueVisible(TRUE);
+        }
+
+        $em->flush();
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
